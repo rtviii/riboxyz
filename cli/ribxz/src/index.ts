25f991bc5e7a9e4e0cc312e65fdab1a660f0ba23
Artem <rtkushner@gmail.com>
2022-12-05 13:14:07 -0800

    seamless env overriding

--- cli/ribxz/src/index.ts
+++ cli/ribxz/src/index.ts
@@ -7,17 +7,9 @@ export { run } from '@oclif/core'
 
 
 export abstract class BaseCommand extends Command {
+    public neo4j_vars = ["NEO4J_URI", "NEO4J_USER", "NEO4J_PASSWORD", "NEO4J_CURRENTDB"]
     constructor(argv: string[], config: any) {
         super(argv, config);
-        const env_vars = [
-            'NEO4J_URI',
-            "NEO4J_USER",
-            "NEO4J_PASSWORD",
-            "NEO4J_CURRENTDB",
-        ];
-        for (var ee of env_vars) {
-            this.log(`env: ${ee} = ${process.env[ee]}`);
-        }
     }
 
     static globalFlags = {
@@ -26,43 +18,46 @@ export abstract class BaseCommand extends Command {
             char: 'a',
             multiple: false,                             // allow setting this flag multiple times
             env: 'NEO4J_URI',                              // default to value of environment variable
-            parse: async (input) => (process.env["NEO4J_URI"] = input),              // instead of the user input, return a different value
         }),
 
         NEO4J_PASSWORD: Flags.string({
             char: 'p',
             multiple: false,                             // allow setting this flag multiple times
             env: 'NEO4J_PASSWORD',                              // default to value of environment variable
-            parse: async (input) => (process.env["NEO4J_PASSWORD"] = input),              // instead of the user input, return a different value
         }),
 
         NEO4J_USER: Flags.string({
             char: 'u',
             multiple: false,                             // allow setting this flag multiple times
             env: 'NEO4J_USER',                              // default to value of environment variable
-            parse: async (input) => (process.env["NEO4J_USER"] = input),              // instead of the user input, return a different value
         }),
         NEO4J_CURRENTDB: Flags.string({
             char: 'd',
             multiple: false,                             // allow setting this flag multiple times
             env: 'NEO4J_CURRENTDB',                              // default to value of environment variable
-            parse: async (input) => (process.env["NEO4J_CURRENTDB"] = input),              // instead of the user input, return a different value
         }),
         env: Flags.string({
             char: 'e',
             description: 'Environment variable',
-            parse: async (input: string) => {
-                process.env[input.split("=")[0]] = input.split("=")[1]
-                return input
-            }
         }),
     };
 
 
-    async run(){
-        let {flags}  =await this.parse(BaseCommand)
-        console.log("flags", flags)
-        console.log("DDDDDDDD")
+    async run() {
+        let { flags } = await this.parse(BaseCommand)
+        for (var v of ["NEO4J_URI", "NEO4J_USER", "NEO4J_PASSWORD", "NEO4J_CURRENTDB"]) {
+            if (Object.keys(flags).includes(v)) {
+                process.env[v] = flags[v]
+            }
+        }
+        if (flags.env) {
+            const varstring = flags.env
+            process.env[varstring.split("=")[0]] = varstring.split("=")[1]
+        }
 
+        for (var ee of this.neo4j_vars) {
+            this.log(`env: ${ee} = ${process.env[ee]}`);
+        }
     }
+
 }
