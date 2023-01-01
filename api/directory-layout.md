891ba72c6f41023eccdf1f6ac72909d06962b975
Artem <rtkushner@gmail.com>
2023-01-01 01:42:56 -0800

    api

---
+++ api/directory-layout.md
@@ -0,0 +1,23 @@
+# Directory Layout
+
+`comp` -- computational modules and scripts ( ex. acting locally on structures via biopython or pymol )
+
+`db` -- interactions with the database
+
+`utils` -- any utilities not directly related to primary: statistics, ingress/egress, diagnostics
+
+`rbxz_bend` -- main module
+
+# Important Variables
+
+
+# TODOs:
+
+- ...see main urls
+- move thumbnails to RIBETL_DATA
+- define ingress scripts in backend dockerfile even if unused
+
+
+
+
+
---
+++ api/mod_comp/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+import os
+from pymol import cmd
+from rbxz_bend.settings import RIBETL_DATA
+from rbxz_bend.settings import INGRESS_EXEC
+from subprocess import Popen, PIPE, STDOUT, run
+
+#-⋯⋯⋅⋱⋰⋆⋅⋅⋄⋅⋅∶⋅⋅⋄▫▪▭┈┅✕⋅⋅⋄⋅⋅✕∶⋅⋅⋄⋱⋰⋯⋯⋯⋯⋅⋱⋰⋆⋅⋅⋄⋅⋅∶⋅⋅⋄▫▪▭┈┅✕⋅⋅⋄⋅⋅✕∶⋅⋅⋄⋱⋰⋯⋯⋯⋅⋱⋰⋆⋅⋅⋄⋅⋅∶⋅⋅⋄▫▪▭┈┅✕⋅⋅⋄⋅⋅✕∶⋅⋅⋄⋱⋰⋯⋯⋯
+@api_view(['GET'])
+def hello(request):
+    return Response("Hello from comp")
+
+@api_view(['GET'])
+def edit_chain(request):
+    params     = dict(request.GET)
+    structid   = str.upper(params['structid'][0])
+    filehandle = os.path.join(RIBETL_DATA, structid + ".cif")
+
+    #Clip chains with pymol, create snippet objects, align those and save.
+    cmd.load(filehandle)
+    cmd.select("resi 1-3")
+    cmd.create("{}_test".format(structid), "sele")
+    tosave=os.path.join(RIBETL_DATA, f"tempchain{structid}.cif")
+    print("Saving to ", tosave)
+    cmd.save(tosave)
+
+    return Response("Edited chain successfully")
+
