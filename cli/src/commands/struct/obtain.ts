3207881d93093857fe7d66159d446d88453f8462
Artem <rtkushner@gmail.com>
2023-01-05 00:53:48 -0800

    hi

--- cli/src/commands/struct/obtain.ts
+++ cli/src/commands/struct/obtain.ts
@@ -1,17 +1,14 @@
 import { Flags } from "@oclif/core";
 import { exec } from "node:child_process";
 import { BaseCommand } from "../..";
-import { queryStructDb, StructureFolder } from "../../structure_processing/structure";
+import { commit_struct_to_Db, queryStructDb, StructureFolder } from "../../structure_processing/structure";
 import { RibosomeStructure } from "../../structure_processing/structure_types";
 
 export default class Obtain extends BaseCommand {
-
     static description = 'Query structure in the database'
     static flags = {
-
-    repair: Flags.boolean({ char: 'R' }),
-    commit: Flags.boolean({ char: 'C' }),
-
+        repair: Flags.boolean({ char: 'R' }),
+        commit: Flags.boolean({ char: 'C' }),
     }
 
     static args = [
@@ -23,24 +20,11 @@ export default class Obtain extends BaseCommand {
         const rcsb_id = String(args.rcsb_id).toUpperCase();
         this.log(`Obtaining structure ${rcsb_id}`)
         let x = new StructureFolder(rcsb_id, true)
-            await x.initialize_assets(flags.repair)
-            await x.initialize_ligands(flags.repair, x.structure as RibosomeStructure )
+        await x.initialize_assets(flags.repair)
+        await x.initialize_ligands(flags.repair, x.structure as RibosomeStructure)
 
         if (flags.commit) {
-          this.log(`Commiting ${rcsb_id} to the database`)
-          const commit_script = process.env["COMMIT_STRUCTURE_SH"]
-          let   current_db    = process.env["NEO4J_CURRENTDB"]
-          let   uri           = process.env["NEO4J_URI"]
-          let   invocation    = `${commit_script} -s ${rcsb_id} -d ${current_db} -a "${uri}"`
-          let   cp            = exec(invocation)
-          if (cp.stderr !== null) {
-            cp.stderr.on("data",
-              (data) => {
-                console.log(data)
-              }
-            )
-          }
-          cp.stdout?.on("data", (data) => { console.log(data) })
+            commit_struct_to_Db(rcsb_id)
         }
 
     }
