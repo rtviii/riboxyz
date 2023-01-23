d91e7f6d6a64bd24c3feb17c9c86a8217fcea9ad
Artem <rtkushner@gmail.com>
2023-01-23 12:50:19 -0800

    fuzzy finding appears a lot more robust

--- cli/scripts/pml_ptc_vis.py
+++ cli/scripts/pml_ptc_vis.py
@@ -4,19 +4,23 @@ from pprint import pprint
 from typing import List
 from pymol import cmd
 
+
+RIBETL_DATA = "/home/rxz/dev/static"
 def build_selection_string(site_name: str, chain_name: str, res_ids: List[int]):
     """pymol command being issued"""
-    selection_name    = '23S_{}_{}'.format(chain_name, site_name)
-    selection_string  = f"c. {chain_name} and "
+    selection_name = '23S_{}_{}'.format(chain_name, site_name)
+    selection_string = f"c. {chain_name} and "
     selection_string += " OR ".join([*map(lambda x: f"resi {x}", res_ids)])
     return selection_name, selection_string
 
-def highlight_ptc(structure:str):
+
+def highlight_ptc(structure: str):
     structure = structure.upper()
     ptc_coord_path = os.path.join(
-                "/home/rxz/dev/docker_ribxz/cli/scripts/PTC_COORDINATES", 
-                f'{structure}_PTC.json'
-            )
+        RIBETL_DATA,
+        "PTC_COORDINATES",
+        f'{structure}_PTC.json'
+    )
     with open(ptc_coord_path, 'r') as f:
         ptc = json.load(f)
         chain = [*ptc.keys()][0]
@@ -37,7 +41,6 @@ def highlight_ptc(structure:str):
         name9, selection9 = build_selection_string(
             "site_9", chain, [*map(lambda truple: truple[0], ptc[chain]["site_9"])])
 
-       
         cmd.create(name6, selection6)
         cmd.create(name8, selection8)
         cmd.create(name9, selection9)
@@ -55,16 +58,70 @@ def highlight_ptc(structure:str):
         cmd.bg_color("white")
         cmd.reset()
 
+
+def highlight_ptc_fuzzy(structure: str):
+    structure = structure.upper()
+    print("GOT HERE")
+    print(RIBETL_DATA)
+    ptc_fuzzy_coord_path = os.path.join(
+        RIBETL_DATA,
+        "PTC_COORDINATES",
+        f'{structure}_FUZZY_PTC.json'
+    )
+    print(ptc_fuzzy_coord_path)
+    with open(ptc_fuzzy_coord_path, 'r') as f:
+        ptc = json.load(f)
+        chain = [*ptc.keys()][0]
+        if chain == None:
+            exit("Could not identify chain")
+
+        name6, selection6 = build_selection_string(
+            "site_6", chain, ptc[chain]["site_6"])
+        name8, selection8 = build_selection_string(
+            "site_8", chain, ptc[chain]["site_8"])
+        name9, selection9 = build_selection_string(
+            "site_9", chain, ptc[chain]["site_9"])
+
+        cmd.create(name6, selection6)
+        cmd.create(name8, selection8)
+        cmd.create(name9, selection9)
+
+        cmd.color("gray60", structure)
+
+        cmd.color("forest", name6)
+        cmd.color("tv_orange", name8)
+        cmd.color("blue", name9)
+
+        cmd.show("surface", name6)
+        cmd.show("surface", name8)
+        cmd.show("surface", name9)
+        cmd.set("cartoon_transparency", 0.5)
+        cmd.bg_color("white")
+        cmd.reset()
+
+
 def list_bacteria():
-    pprint(os.listdir("/home/rxz/dev/docker_ribxz/cli/scripts/PTC_COORDINATES"))
+    pprint(os.listdir(f"{RIBETL_DATA}/PTC_COORDINATES"))
 
 
-def ptc(struct:str):
+def ptc(struct: str):
     cmd.delete("all")
-    struct      = struct.upper()
-    struct_path = os.path.join("/home/rxz/dev/static/{}/{}.cif".format(struct,struct))
+    struct = struct.upper()
+    struct_path = os.path.join(
+        "/home/rxz/dev/static/{}/{}.cif".format(struct, struct))
     cmd.load(struct_path)
     highlight_ptc(struct)
 
-cmd.extend("ptc",ptc)
-cmd.extend("list_bacteria",list_bacteria)
+
+def ptc_fuzzy(struct: str):
+    cmd.delete("all")
+    struct = struct.upper()
+    struct_path = os.path.join(
+        "/home/rxz/dev/static/{}/{}.cif".format(struct, struct))
+    cmd.load(struct_path)
+    highlight_ptc_fuzzy(struct)
+
+
+# cmd.extend("ptc",ptc)
+cmd.extend("ptc", ptc_fuzzy)
+cmd.extend("list_bacteria", list_bacteria)
