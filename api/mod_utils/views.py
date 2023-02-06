d8502c7f5df3a388342e5127d5c75ff6349d1c77
Artem <rtkushner@gmail.com>
2023-02-05 21:06:10 -0800

    installed requests

--- api/mod_utils/views.py
+++ api/mod_utils/views.py
@@ -4,10 +4,12 @@ from rbxz_bend.neoget import _neoget
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 import os
-from rbxz_bend.settings import  NEO4J_CURRENTDB, NEO4J_PASSWORD, NEO4J_URI, NEO4J_USER, RIBETL_DATA, CYPHER_EXEC
-from neo4j import GraphDatabase,Driver,Session, Transaction, Result, ResultSummary
+from rbxz_bend.settings import NEO4J_CURRENTDB, NEO4J_PASSWORD, NEO4J_URI, NEO4J_USER, RIBETL_DATA, CYPHER_EXEC
+from neo4j import GraphDatabase, Driver, Session, Transaction, Result, ResultSummary
 from subprocess import Popen, PIPE, STDOUT, run
-#-⋯⋯⋅⋱⋰⋆⋅⋅⋄⋅⋅∶⋅⋅⋄▫▪▭┈┅✕⋅⋅⋄⋅⋅✕∶⋅⋅⋄⋱⋰⋯⋯⋯⋯⋅⋱⋰⋆⋅⋅⋄⋅⋅∶⋅⋅⋄▫▪▭┈┅✕⋅⋅⋄⋅⋅✕∶⋅⋅⋄⋱⋰⋯⋯⋯⋅⋱⋰⋆⋅⋅⋄⋅⋅∶⋅⋅⋄▫▪▭┈┅✕⋅⋅⋄⋅⋅✕∶⋅⋅⋄⋱⋰⋯⋯⋯
+import requests
+from urllib import parse
+# -⋯⋯⋅⋱⋰⋆⋅⋅⋄⋅⋅∶⋅⋅⋄▫▪▭┈┅✕⋅⋅⋄⋅⋅✕∶⋅⋅⋄⋱⋰⋯⋯⋯⋯⋅⋱⋰⋆⋅⋅⋄⋅⋅∶⋅⋅⋄▫▪▭┈┅✕⋅⋅⋄⋅⋅✕∶⋅⋅⋄⋱⋰⋯⋯⋯⋅⋱⋰⋆⋅⋅⋄⋅⋅∶⋅⋅⋄▫▪▭┈┅✕⋅⋅⋄⋅⋅✕∶⋅⋅⋄⋱⋰⋯⋯⋯
 
 
 @api_view(['GET'])
@@ -26,23 +28,63 @@ def last_update(request):
     return Response()
 
 
-#-------------------- SRUCT SINGLE
+# -------------------- SRUCT SINGLE
 @api_view(['GET'])
 def struct_commit_new_PDB(request):
-    params  = dict(request.GET)
+    params = dict(request.GET)
     rcsb_id = str.upper(params['rcsb_id'][0])
-    proc    = Popen([ "ribxzcli",  "struct", "obtain", f"{rcsb_id}", "--commit"], env=os.environ.copy(), stdout=PIPE)
+    proc = Popen(["ribxzcli",  "struct", "obtain",
+                 f"{rcsb_id}", "--commit"], env=os.environ.copy(), stdout=PIPE)
     print([proc.stdout, proc.stdin, proc.stderr])
     sys.stdout.flush()
-    return Response([proc.stdout, proc.stdin, pro.stderr])
+    return Response([proc.stdout, proc.stdin, proc.stderr])
 
-#-------------------- SRUCTS PLURAL
+# -------------------- SRUCTS PLURAL
+
+
 @api_view(['GET'])
-def structs_get_ids(request):
+def structs_get_all_ids(request):
+
     with GraphDatabase.driver(NEO4J_URI, auth=(NEO4J_USER, NEO4J_PASSWORD)) as driver:
-        def transaction_fn(tx:Transaction):
-             r = tx.run("match (r:RibosomeStructure) return r.rcsb_id;")
-             return r.values()
+        def transaction_fn(tx: Transaction):
+            r = tx.run("match (r:RibosomeStructure) return r.rcsb_id;")
+            return r.values()
         with driver.session() as session:
             values = session.read_transaction(transaction_fn)
             return Response([v[0] for v in values])
+
+
+@api_view(['GET'])
+def structs_diff_pdb(request):
+
+    rcsb_search_api = "https://search.rcsb.org/rcsbsearch/v2/query"
+    params = {
+        "query": {
+            "type": "group",
+            "logical_operator": "and",
+            "nodes": [
+                {
+                    "type": "group",
+                    "logical_operator": "and",
+                    "nodes": [
+                        {"type": "group", "logical_operator": "and", "nodes": [{"type": "terminal", "service": "text", "parameters": {
+                            "operator": "contains_phrase", "negation": False, "value": "RIBOSOME", "attribute": "struct_keywords.pdbx_keywords"}}]},
+                        {"type": "group", "logical_operator": "and", "nodes": [{"type": "terminal", "service": "text", "parameters": {
+                            "operator": "greater", "negation": False, "value": 25, "attribute": "rcsb_entry_info.polymer_entity_count_protein"}}]},
+                    ],
+                    "label": "text"
+                }
+            ],
+            "label": "query-builder"
+        },
+        "return_type": "entry",
+        "request_options": {
+            "return_all_hits": True,
+            "results_verbosity": "compact"
+        }
+    }
+    query_response = requests.get(
+        rcsb_search_api, params=parse.urlencode(params))
+    query_response = query_response.json()
+    print(query_response)
+    return Response(query_response)
