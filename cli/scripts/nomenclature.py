0a8ababddca326aeaef77e0a5464b57397ee7238
Artem <rtkushner@gmail.com>
2023-02-20 23:00:33 -0800

    comparing entropies across few controls

--- cli/scripts/nomenclature.py
+++ cli/scripts/nomenclature.py
@@ -1,4 +1,7 @@
 import argparse
+import os
+import subprocess
+import sys
 import prody as prd
 from prody import calcShannonEntropy
 
@@ -21,18 +24,34 @@ from prody import calcShannonEntropy
 # RESSURECT_PATH               = args.resurrect if args.resurrect is not None else 0
 
 
+def muscle_combine_profiles(msa_path1: str, msa_path2: str, out_filepath: str):
+    """Combine two MSA-profiles into a single one. Used here to "append" a target sequence two the ribovision alignment. """
+    cmd = ['/home/rxz/dev/docker_ribxz/cli/scripts/muscle3.8', '-profile','-in1', msa_path1, '-in2', msa_path2, '-out', out_filepath]
+    subprocess.Popen(cmd, stdout=subprocess.PIPE,stderr=subprocess.PIPE, env=os.environ.copy()).wait()
+    sys.stdout.flush()
 
 def barr2str (bArr):
-
     return ''.join([ x.decode("utf-8") for x in bArr])
     
 
 # open msa file with prody
 msa_file = prd.parseMSA('PV_uL2.fasta')
-s        = calcShannonEntropy(msa_file, omitgaps=True)
-print(sum(s))
+s        = calcShannonEntropy(msa_file, omitgaps=False)
+print("Unpetrurbed entropy: {}".format(sum(s)))
+suspects = [ 
+'bL34',
+'bS2',
+'uL2',
+'uS3',
+'uS5'
+ ]
+
+paths = [*map(lambda _: "5afi_unclassified_{}.fasta".format(_),suspects)]
+for control in suspects:
+    control_path = (lambda _: "5afi_unclassified_{}.fasta".format(_))(control)
+    muscle_combine_profiles('PV_uL2.fasta', control_path, 'PV_uL2_with_{}.fasta'.format(control))
 
 
-
-
-
+def compare_entropies(msa_path1: str, msa_path2: str):
+    msa_file = prd.parseMSA('PV_uL2.fasta')
+    msa_file_ctl = prd.parseMSA('PV_uL2.fasta')
