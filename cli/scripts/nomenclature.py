16c322cfb810c47b55bc9454b3761ec73d3b9963
Artem <rtkushner@gmail.com>
2023-02-21 00:41:27 -0800

    do not omit gaps

--- cli/scripts/nomenclature.py
+++ cli/scripts/nomenclature.py
@@ -5,25 +5,6 @@ import sys
 import prody as prd
 from prody import calcShannonEntropy
 
-# parser = argparse.ArgumentParser (description='Ribosome Cli')
-
-# #TODO:
-# # - SEPARATE LIB CODE from CLI CODE
-# # - retype the typescript profile code in python
-
-# parser.add_argument ("-it"       , "--itern"               , type= int      ,                 help = "The number of iterations"                                                                                            )
-# # parser.add_argument ("-itstart"  , "--iter_start"              , type   = int          ,required =True,                 help = "The number of iterations"                                                                                                                                )
-# # parser.add_argument ("-itend"    , "--iter_end"                , type   = int          ,required =True,                 help = "The number of iterations"                                                                                                                                )
-# args                         = parser .parse_args()
-
-# GENERATION 					 = 1000
-# ITSTART                      = int(args.iter_start)
-# ITEND                        = int(args.iter_end)
-# REPLICATE_N                  = int (args .siminst if args.siminst is not None else 0)
-# OUTDIR                       = args.outdir if args.outdir is not None else 0
-# RESSURECT_PATH               = args.resurrect if args.resurrect is not None else 0
-
-
 def muscle_combine_profiles(msa_path1: str, msa_path2: str, out_filepath: str):
     """Combine two MSA-profiles into a single one. Used here to "append" a target sequence two the ribovision alignment. """
     cmd = ['/home/rxz/dev/docker_ribxz/cli/scripts/muscle3.8', '-profile','-in1', msa_path1, '-in2', msa_path2, '-out', out_filepath]
@@ -33,11 +14,18 @@ def muscle_combine_profiles(msa_path1: str, msa_path2: str, out_filepath: str):
 def barr2str (bArr):
     return ''.join([ x.decode("utf-8") for x in bArr])
     
+def compare_entropies(msa_path1: str, msa_path2: str):
+    msa_file     = prd.parseMSA(msa_path1)
+    msa_file_ctl = prd.parseMSA(msa_path2)
+    H            = sum(calcShannonEntropy(msa_file, omitgaps=False))
+    H_w_ctl      = sum(calcShannonEntropy(msa_file_ctl, omitgaps=False))
+    return  H, H_w_ctl
 
-# open msa file with prody
-msa_file = prd.parseMSA('PV_uL2.fasta')
-s        = calcShannonEntropy(msa_file, omitgaps=False)
-print("Unpetrurbed entropy: {}".format(sum(s)))
+prd.confProDy(verbosity='none')
+
+chief_msa_path = 'PV_uS5.fasta'
+chief_msa      = prd.parseMSA(chief_msa_path)
+
 suspects = [ 
 'bL34',
 'bS2',
@@ -47,27 +35,15 @@ suspects = [
  ]
 
 paths = [*map(lambda _: "5afi_unclassified_{}.fasta".format(_),suspects)]
+
 for control in suspects:
     control_path = (lambda _: "5afi_unclassified_{}.fasta".format(_))(control)
-    muscle_combine_profiles('PV_uL2.fasta', control_path, 'PV_uL2_with_{}.fasta'.format(control))
-
-# silence prody
-
-prd.confProDy(verbosity='none')
-
-def compare_entropies(msa_path1: str, msa_path2: str):
-    msa_file     = prd.parseMSA(msa_path1)
-    msa_file_ctl = prd.parseMSA(msa_path2)
-    H            = sum(calcShannonEntropy(msa_file, omitgaps=False))
-    H_w_ctl      = sum(calcShannonEntropy(msa_file_ctl, omitgaps=False))
-
-    return  H, H_w_ctl
-    
-  
+    muscle_combine_profiles(chief_msa_path, control_path, '{}with_{}.fasta'.format(chief_msa_path,control))
 
 for control in suspects:
     print("---------")
-    print(f"Comparing 'PV_uL2.fasta' against 'PV_uL2_with_{control}.fasta'") 
-    h, hwctl = compare_entropies('PV_uL2.fasta', f'PV_uL2_with_{control}.fasta')
+    # print(f"Comparing 'PV_uL2.fasta' against 'PV_uL2_with_{control}.fasta'") 
+    print(f"Comparing 'PV_uS5.fasta' against 'PV_uS5_with_{control}.fasta'") 
+    h, hwctl = compare_entropies(chief_msa_path, '{}with_{}.fasta'.format(chief_msa_path,control))
     print("Unpetrurbed entropy: {}".format(h))
     print("Perturbed entropy: {}".format(hwctl))
