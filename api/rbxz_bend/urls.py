a488752c2de29226e364b665a0e74f2c985c9b61
Artem <rtkushner@gmail.com>
2023-03-02 17:17:31 -0800

    switching to ninja

--- api/rbxz_bend/urls.py
+++ api/rbxz_bend/urls.py
@@ -2,8 +2,11 @@ from django.conf import settings
 from django.urls import include, path
 from django.contrib import admin
 from django.conf.urls.static import static
+from rbxz_bend.api import router
 # TODO: retrofit old urls to the new site.
-
+from ninja import NinjaAPI
+api = NinjaAPI()
+api.add_router('/blog/',router)
 
 
 
@@ -13,6 +16,7 @@ urlpatterns = [
     path('db/'    , include('mod_db.urls', 'mod_db')      ),
     path('comp/'  , include('mod_comp.urls', 'mod_comp')  ),
     path('utils/' , include('mod_utils.urls', 'mod_utils')),
+    path('api/',api.urls)
     ]
 
 # urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
