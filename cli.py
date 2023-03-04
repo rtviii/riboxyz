68d60996d03214653613b370f816e85b20c51e06
Artem <rtkushner@gmail.com>
2023-03-04 10:42:58 -0800

    before merge

--- cli.py
+++
@@ -1,9 +0,0 @@
-from pprint import pprint
-from ribctl.lib.types.types_ribosome import RibosomeAssets, RibosomeStructure
-
-
-# To generate types:
-# pydantic2ts --module ribctl/lib/types/types_ribosome.py --output ./types_ribosome.ts
-r = RibosomeStructure.from_json_profile("4UG0")
-pprint(r.rnas[0].dict())
-      
