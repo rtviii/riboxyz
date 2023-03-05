335068745314b93c02ed2c6126654a9b71fcff71
Artem <rtkushner@gmail.com>
2023-03-05 10:35:29 -0800

    adapter works

--- api/ribctl/db/constraints.py
+++ api/ribctl/db/constraints.py
@@ -1,16 +1,15 @@
 from typing import LiteralString
 from neo4j import  Driver, ManagedTransaction, Transaction
-from api.ribctl.db.proteins import node__protein_class
-from api.ribctl.db.rna import node__rna_class
-from api.ribctl.lib.types.types_polymer import list_LSU_Proteins, list_SSU_Proteins, list_RNAClass
+from ribctl.db.proteins import node__protein_class
+from ribctl.db.rna import node__rna_class
+from ribctl.lib.types.types_polymer import list_LSU_Proteins, list_SSU_Proteins, list_RNAClass
 
-NODE_CONSTRAINTS :list[LiteralString]=[
+NODE_CONSTRAINTS:list[LiteralString]=[
     """CREATE CONSTRAINT IF NOT EXISTS ON (ipro:InterProFamily) ASSERT ipro.family_id  IS UNIQUE;""",
     """CREATE CONSTRAINT IF NOT EXISTS ON (go:GOClass) ASSERT go.class_id IS UNIQUE;""",
     """CREATE CONSTRAINT IF NOT EXISTS ON (q:RibosomeStructure) Assert q.rcsb_id IS UNIQUE;""",
     """CREATE CONSTRAINT IF NOT EXISTS ON (pf:PFAMFamily) assert pf.family_id  is unique;""",
     """CREATE CONSTRAINT IF NOT EXISTS ON (lig:Ligand) assert lig.chemicalId is unique;""",
-    """CREATE CONSTRAINT IF NOT EXISTS ON (nc:NomenclatureClass) assert nc.class_id is unique;""",
     """CREATE CONSTRAINT IF NOT EXISTS ON (nc:NomenclatureClass) assert nc.class_id is unique;""",
 ]
 
@@ -23,9 +22,3 @@ def init_rna_classes(driver:Driver):
     with driver.session() as s:
         for rna_class in list_RNAClass:
             s.execute_write(node__rna_class(rna_class))
-
-
-def uw__get_all_constraints(tx: Transaction | ManagedTransaction):
-    return tx.run("""//
-    CALL db.constraints;
-    """)
