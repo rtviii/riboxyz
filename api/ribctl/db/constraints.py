2476f03360fda30585d01edfbe7dbfef133281e3
Artem <rtkushner@gmail.com>
2023-03-04 22:14:09 -0800

    constraints creation

--- api/ribctl/db/constraints.py
+++ api/ribctl/db/constraints.py
@@ -1,8 +1,18 @@
+from typing import LiteralString
 from neo4j import  Driver, ManagedTransaction, Transaction
 from api.ribctl.db.proteins import node__protein_class
 from api.ribctl.db.rna import node__rna_class
-from ribctl.lib.types.types_polymer import list_LSU_Proteins, list_SSU_Proteins, list_RNAClass
+from api.ribctl.lib.types.types_polymer import list_LSU_Proteins, list_SSU_Proteins, list_RNAClass
 
+NODE_CONSTRAINTS :list[LiteralString]=[
+    """CREATE CONSTRAINT IF NOT EXISTS ON (ipro:InterProFamily) ASSERT ipro.family_id  IS UNIQUE;""",
+    """CREATE CONSTRAINT IF NOT EXISTS ON (go:GOClass) ASSERT go.class_id IS UNIQUE;""",
+    """CREATE CONSTRAINT IF NOT EXISTS ON (q:RibosomeStructure) Assert q.rcsb_id IS UNIQUE;""",
+    """CREATE CONSTRAINT IF NOT EXISTS ON (pf:PFAMFamily) assert pf.family_id  is unique;""",
+    """CREATE CONSTRAINT IF NOT EXISTS ON (lig:Ligand) assert lig.chemicalId is unique;""",
+    """CREATE CONSTRAINT IF NOT EXISTS ON (nc:NomenclatureClass) assert nc.class_id is unique;""",
+    """CREATE CONSTRAINT IF NOT EXISTS ON (nc:NomenclatureClass) assert nc.class_id is unique;""",
+]
 
 def init_protein_classes(driver:Driver):
     with driver.session() as s:
@@ -14,18 +24,8 @@ def init_rna_classes(driver:Driver):
         for rna_class in list_RNAClass:
             s.execute_write(node__rna_class(rna_class))
 
-def create_constraints(tx: Transaction | ManagedTransaction):
-    tx.run("""//
-    CREATE CONSTRAINT IF NOT EXISTS ON (ipro:InterProFamily) ASSERT ipro.family_id  IS UNIQUE;
-    CREATE CONSTRAINT IF NOT EXISTS ON (go:GOClass) ASSERT go.class_id IS UNIQUE;
-    CREATE CONSTRAINT IF NOT EXISTS ON (q:RibosomeStructure) Assert q.rcsb_id IS UNIQUE;
-    CREATE CONSTRAINT IF NOT EXISTS ON (pf:PFAMFamily) assert pf.family_id  is unique;
-    CREATE CONSTRAINT IF NOT EXISTS ON (lig:Ligand) assert lig.chemicalId is unique;
-    CREATE CONSTRAINT IF NOT EXISTS ON (nc:NomenclatureClass) assert nc.class_id is unique;
-    CREATE CONSTRAINT IF NOT EXISTS ON (nc:NomenclatureClass) assert nc.class_id is unique;
-    """)
 
-def get_all_constraints(tx: Transaction | ManagedTransaction):
+def uw__get_all_constraints(tx: Transaction | ManagedTransaction):
     return tx.run("""//
     CALL db.constraints;
     """)
--- api/ribctl/db/driver.py
+++ api/ribctl/db/driver.py
@@ -1,24 +1,11 @@
-from typing import Self
-from neo4j import Driver, GraphDatabase
+from ast import List
+from typing import Dict, LiteralString, Self
+from neo4j import Driver, GraphDatabase, Transaction
+from pyparsing import Any
+from api.ribctl.db.constraints import NODE_CONSTRAINTS
 
 """Functions of the form create_node_xxxx return a closure over their target [xxxx] because the neo4j expects a 'unit-of-work'
 function that takes a transaction as its argument."""
-
-def init_driver(uri:str="neo4j://localhost:7687", username:str="neo4j", password="neo4j"):
-    # TODO: Create an instance of the driver here
-    api = GraphDatabase.driver(uri, auth=(username, password))
-    return api
-
-
-driver = init_driver("neo4j://localhost:7687", "neo4j", "neo4j")
-
-class Neo4jDB():
-
-    driver:Driver 
-
-    def __init__(self, uri=None, username=None, password=None)->None:
-        self.driver = init_driver(uri, username,password)
-        return self
 
 # ※ ----------------[ 0.Database  inits: constraints & nomenclature classes]
 # ※ ----------------[ 1.Structure Nodes                                    ]
@@ -26,5 +13,34 @@ class Neo4jDB():
 # ※ ----------------[ 3.Protein   Nodes                                    ]
 # ※ ----------------[ 4.Ligand    Nodes                                    ] 
 # ※ ----------------[ 4. Ingress]
+
+def init_driver(uri:str="neo4j://localhost:7687", username:str="neo4j", password="neo4j"):
+    # TODO: Create an instance of the driver here
+    api = GraphDatabase.driver(uri, auth=(username, password))
+    return api
+
+driver = init_driver("neo4j://localhost:7687", "neo4j", "neo4j")
+
+class Neo4jDB():
+
+    driver:Driver 
+    def __init__(self, **kwargs)->None:
+        self.driver = init_driver(**kwargs)
+
+    def see_constraints(self)->list[dict[str,Any]]:
+        with self.driver.session() as s:
+            r = s.run("""//
+            CALL db.constraints;
+                  """)
+            return r.data()
+
+    def init_constraints(self)->None:
+        with self.driver.session() as s:
+            tx:Transaction
+            for c in NODE_CONSTRAINTS:
+                s.write_transaction(lambda tx: tx.run(c))
+                
+
+
 
 
