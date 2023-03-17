39889bdb762dde6ecb891e79526062b220004e08
Artem <rtkushner@gmail.com>
2023-03-16 17:58:11 -0700

    some code shuffling

--- api/rbxz_bend/api/router_v0.py
+++ api/rbxz_bend/api/router_v0.py
@@ -4,14 +4,13 @@ from ninja import Router
 from rbxz_bend.settings import get_logger
 from ribctl.lib.struct_rcsb_api import current_rcsb_structs
 from ribctl.lib.types.types_ribosome_assets import RibosomeAssets
-from ribctl.db.ribosomexyz import riboxyzDB
 from ribctl.lib.types.types_polymer import RNAClass
 from ribctl.lib.types.types_ribosome import ExogenousRNAByStruct, ProteinClass, RibosomeStructure
 from ribctl.db.data import QueryOps
 from schema.v0 import BanClassMetadata, LigandInstance, LigandlikeInstance, NeoStruct, NomenclatureClass, NomenclatureClassMember
 
 v0 = Router()
-QO = QueryOps()
+from .api import qo 
 
 
 
@@ -20,63 +19,63 @@ QO = QueryOps()
 
 @v0.get('/get_all_structures', response=list[NeoStruct], tags=['Structure'])
 def get_all_structures(request,):
-    return QO.get_all_structures()
+    return qo.get_all_structures()
 
 @v0.get('/get_struct', response=NeoStruct, tags=['Structure'])
 def get_struct(request,rcsb_id:str):
-    return QO.get_struct(rcsb_id.upper())
+    return qo.get_struct(rcsb_id.upper())
 
 @v0.get('/get_full_structure', response=NeoStruct, tags=['Structure'])
 def get_full_structure(request,rcsb_id:str):
-    return QO.get_struct(rcsb_id.upper())
+    return qo.get_struct(rcsb_id.upper())
 
 @v0.get('/get_all_ligands', response=list[NeoStruct], tags=['Ligand'])
 def get_all_ligands(request,):
-    return QO.get_all_ligands()
+    return qo.get_all_ligands()
 
 @v0.get('/get_individual_ligand', response=list[LigandInstance], tags=['Ligand'])
 def get_individual_ligand(request,chemicalId:str):
-    return QO.get_individual_ligand(chemicalId)
+    return qo.get_individual_ligand(chemicalId)
     
 @v0.get('/get_all_ligandlike', response=list[LigandlikeInstance], tags=['Ligand'])
 def get_all_ligandlike(request,):
-    return QO.get_all_ligandlike()
+    return qo.get_all_ligandlike()
 
 @v0.get('/get_RibosomeStructure', response=RibosomeStructure, tags=['Structure'])
 def get_RibosomeStructure(request,rcsb_id:str):
-    return QO.get_RibosomeStructure(rcsb_id.upper())
+    return qo.get_RibosomeStructure(rcsb_id.upper())
 
 @v0.get('/match_structs_w_proteins', response=RibosomeStructure, tags=['Structure'])
 def match_structs_w_proteins(request,has_proteins:list[ProteinClass]):
-    return QO.match_structs_w_proteins(has_proteins)
+    return qo.match_structs_w_proteins(has_proteins)
 
 @v0.get('/get_banclass_for_chain', response=list[ProteinClass], tags=['Classification'])
 def get_banclass_for_chain(request,rcsb_id:str, auth_asym_id:str):
-    return QO.get_banclass_for_chain(rcsb_id,auth_asym_id)
+    return qo.get_banclass_for_chain(rcsb_id,auth_asym_id)
     
 
 @v0.get('/get_banclasses_metadata', response=list[BanClassMetadata], tags=['Classification'])
 def get_banclasses_metadata(request,family:typing.Literal['b','e','u'], subunit:typing.Literal['SSU', 'LSU']):
-    return QO.get_banclasses_metadata(family, subunit)
+    return qo.get_banclasses_metadata(family, subunit)
     
 @v0.get('/get_nom_classes', response=list[NomenclatureClass], tags=['Classification'])
 def get_nom_classes(request,):
-    return QO.list_nom_classes()
+    return qo.list_nom_classes()
 
 @v0.get('/gmo_nom_class', response=list[ NomenclatureClassMember ], tags=['Classification'])
 def gmo_nom_class(request,class_id:ProteinClass):
-    return QO.gmo_nom_class(class_id)
+    return qo.gmo_nom_class(class_id)
 
 @v0.get('/proteins_number', response=int, tags=['Protein'])
 def proteins_number(request):
-    return QO.proteins_number()
+    return qo.proteins_number()
 
 @v0.get('/get_rnas_by_struct', response=list[ExogenousRNAByStruct], tags=['RNA'])
 def get_rnas_by_struct(request):
-    return QO.get_rnas_by_struct()
+    return qo.get_rnas_by_struct()
 
 @v0.get('/get_rna_class', response=list[NomenclatureClassMember], tags=['RNA'])
 def get_rna_class(request,class_id:RNAClass):
-    return QO.get_rna_class(class_id)
+    return qo.get_rna_class(class_id)
 
 
