2316c45f34f14a53cc66b24d664589bd032ba247
Artem <rtkushner@gmail.com>
2023-03-18 20:22:41 -0700

    moved ribosomxyz into backend

--- api/rbxz_bend/db_connection.py
+++ api/rbxz_bend/db_connection.py
@@ -1,23 +1,8 @@
 from neo4j.exceptions import AuthError
-from ribctl.db.ribosomexyz import riboxyzDB
+from rbxz_bend.db.ribosomexyz import riboxyzDB
 from rbxz_bend.settings import NEO4J_PASSWORD, NEO4J_URI, NEO4J_USER
 
-# global db_connection
-
-# try:
-#     print("trying -----------")
-#     db_connection    = riboxyzDB(uri=NEO4J_URI,password=NEO4J_PASSWORD,user=NEO4J_USER)
-
-# except AuthError as e:
-
-#     print("Could not connect to Neo4j database. Trying to change the default password.")
-
-#     _temp_db_connection= riboxyzDB(uri=NEO4J_URI, password="neo4j",user="neo4j")
-#     with _temp_db_connection.driver.session(database='system') as system_s:
-#         r = system_s.run("""ALTER CURRENT USER SET PASSWORD FROM "neo4j" TO "ribosomexyz";""")
-#         print("Changed the default Neo4j password.")
-
-#     print("trying again -----------")
-#     db_connection= riboxyzDB(uri= NEO4J_URI,password = NEO4J_PASSWORD,user= NEO4J_USER)
-db_connection= riboxyzDB(uri= NEO4J_URI,password = NEO4J_PASSWORD,user= NEO4J_USER)
-# db_connection= riboxyzDB(uri=NEO4J_URI, password="neo4j",user="neo4j")
+db_connection = riboxyzDB(uri=NEO4J_URI,
+                          password=NEO4J_PASSWORD,
+                          user=NEO4J_USER
+                          )
--- api/rbxz_bend/urls.py
+++ api/rbxz_bend/urls.py
@@ -1,23 +1,6 @@
-import sys
-import os
-from django.conf import settings
 from django.urls import include, path
-from django.contrib import admin
 from django.conf.urls.static import static
-from ninja import NinjaAPI
 from rbxz_bend.settings import STATIC_ROOT, STATIC_URL
-import asyncio
-import random
-import typing
-from venv import logger
-from ninja import Router
-from rbxz_bend.settings import get_logger
-from ribctl.lib.struct_rcsb_api import current_rcsb_structs
-from ribctl.lib.types.types_ribosome_assets import RibosomeAssets
-from ribctl.db.ribosomexyz import riboxyzDB
-from ribctl.lib.types.types_polymer import RNAClass
-from ribctl.lib.types.types_ribosome import ExogenousRNAByStruct, ProteinClass, RibosomeStructure
-from schema.v0 import BanClassMetadata, LigandInstance, LigandlikeInstance, NeoStruct, NomenclatureClass, NomenclatureClassMember
 from .routers import router_root
 import os
 
