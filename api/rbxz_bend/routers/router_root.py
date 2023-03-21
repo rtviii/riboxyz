4ff48b32fc8a49f15d9397150740b59947f02086
Artem <rtkushner@gmail.com>
2023-03-20 22:33:01 -0700

    docs blocked /

--- api/rbxz_bend/routers/router_root.py
+++ api/rbxz_bend/routers/router_root.py
@@ -2,7 +2,7 @@ from ninja import NinjaAPI
 from .router_v0 import v0
 from .router_test import test
 
-root = NinjaAPI(docs_url='/',
+root = NinjaAPI(docs_url='/docs', openapi_url='/openapi.json',
     title='riboxyz-api',
     description='A programming interface for ribosome data.')
 
