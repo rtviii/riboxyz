35c37f110cc4d0779ef7faa7438867da961a67ca
Artem <rtkushner@gmail.com>
2023-03-20 21:05:31 -0700

    fiddled with inits a little bit

--- api/rbxz_bend/application.py
+++ api/rbxz_bend/application.py
@@ -1,8 +1,9 @@
 from neo4j.exceptions import AuthError
 from rbxz_bend.db.ribosomexyz import ribosomexyzDB
 from rbxz_bend.settings import NEO4J_PASSWORD, NEO4J_URI, NEO4J_USER
+from asyncio import wait, wait_for
 
-db_connection = ribosomexyzDB(uri=NEO4J_URI,
+db_connection =  ribosomexyzDB(uri=NEO4J_URI,
                           password=NEO4J_PASSWORD,
                           user=NEO4J_USER
                           )
