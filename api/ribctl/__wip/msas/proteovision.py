f613c3bce3edddc47a587fa0524f98bcf191e725
Artem <rtkushner@gmail.com>
2023-03-23 18:55:07 -0700

    relabling sequences

--- api/ribctl/__wip/msas/proteovision.py
+++ api/ribctl/__wip/msas/proteovision.py
@@ -1,13 +1,51 @@
+import os
 import typing
 import requests
+from ribctl.lib.types.types_ribosome import ProteinClass
 from  ribctl.lib.types.types_polymer import  list_LSU_Proteins,list_SSU_Proteins
-import sys
 
 
 B            = "2"
 E            = "2759"
 A            = "2157"
 PROTEOVISION_URL = lambda SU, _class, taxids: "https://ribovision3.chemistry.gatech.edu/showAlignment/{}/{}/{}".format(SU,_class,taxids)
+
+
+PROTEOVISION_MSA_FOLDER =  '/home/rxz/dev/docker_ribxz/api/ribctl/__wip/data/msa_classes_proteovision/'
+
+
+def save_aln(protein:ProteinClass, subunit: typing.Literal["SSU","LSU"]):
+        spec_letters, class_digit = protein.split("S" if subunit == "SSU" else "L")
+
+        # correct for the fact that the proteovision API requires two digits for the single-digit classes (i.e uL1 is uL01)
+        if int(class_digit) < 10:
+            class_digit = "0{}".format(class_digit)
+
+        def tax_string(spec_letters):
+            tax_string = ""
+            if 'e' in spec_letters:
+                return E
+            if 'b' in spec_letters:
+                return B
+            if 'u' in spec_letters:
+                return ",".join([B,E,A])
+
+            return tax_string[:-1]
+
+        URI         = PROTEOVISION_URL(subunit,spec_letters + ( "S" if subunit == "SSU" else "L" ) + class_digit, tax_string(spec_letters))
+        resp        = requests.get(URI)
+        start,end   = resp.text.find("<pre>"), resp.text.find("</pre>")
+        fasta_lines = resp.text[start+len("<pre>"):end].replace("&gt;",">")
+        filename    = "{}_ribovision.fasta".format(protein)
+        outpath = os.path.join(PROTEOVISION_MSA_FOLDER,subunit, filename)
+
+        print("{}\t| {} \t| URI: {}".format(protein,resp.status_code, URI) )
+
+
+        if resp.status_code == 200:
+            with open(outpath,"w") as f:
+                f.write(fasta_lines)
+                print("Wrote {} to {}".format(protein,filename))
 
 def save_alns(subunit_protein_names: list[str], subunit: typing.Literal["SSU","LSU"]):
     protnames = subunit_protein_names
@@ -36,13 +74,15 @@ def save_alns(subunit_protein_names: list[str], subunit: typing.Literal["SSU","L
         start,end   = resp.text.find("<pre>"), resp.text.find("</pre>")
         fasta_lines = resp.text[start+len("<pre>"):end].replace("&gt;",">")
         filename    = "{}_ribovision.fasta".format(protein)
+        outpath = os.path.join(PROTEOVISION_MSA_FOLDER,subunit, filename)
 
         print("{}\t| {} \t| URI: {}".format(protein,resp.status_code, URI) )
 
         if resp.status_code == 200:
-            with open(filename,"w") as f:
+            with open(outpath,"w") as f:
                 f.write(fasta_lines)
                 print("Wrote {} to {}".format(protein,filename))
          
 # save_alns(list_LSU_Proteins, "LSU")
-save_alns(list_SSU_Proteins, "SSU")
+# save_alns(list_SSU_Proteins, "SSU")
+save_aln('uL23', "LSU")
