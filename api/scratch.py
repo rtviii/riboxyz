a850677447447a983170b8d43209dc7e1f481ad1
Artem <rtkushner@gmail.com>
2023-04-18 00:46:52 -0700

    taxonomy

---
+++ api/scratch.py
@@ -0,0 +1,26 @@
+from ete3 import NCBITaxa, NodeStyle, TreeStyle
+
+# Create an instance of the NCBITaxa class
+ncbi = NCBITaxa()
+
+#TODO: Ignore intermediate taxa (induct the species, strains, subspecies etc. as nodes)
+# - orgnaism
+# Get the taxonomic IDs of all the taxa in the database
+all_taxa =ncbi.get_descendant_taxa(1)
+# all_taxa = ncbi.get_descendant_taxa(ncbi.get_descendant_taxa(1))
+print()
+
+# Build a tree object representing the taxonomic hierarchy
+# tree = ncbi.get_topology(all_taxa)
+
+# # Customize the appearance of the tree nodes
+# ns = NodeStyle()
+# ns["size"] = 0
+
+# # Create a TreeStyle object to specify the overall layout and appearance of the tree
+# ts = TreeStyle()
+# ts.layout_fn = "layout"
+# ts.mode = "c"
+
+# # Render the tree to a file
+# tree.render("ncbi_taxonomy.png", tree_style=ts)
