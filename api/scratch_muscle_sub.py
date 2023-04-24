cb550aed3091e2a8e2391e6a53f9375d45d77b7e
Artem <rtkushner@gmail.com>
2023-04-24 16:59:01 -0700

    msa extensions

--- api/scratch_muscle_sub.py
+++ api/scratch_muscle_sub.py
@@ -1,51 +1,67 @@
 import os
+from pprint import pprint
 import subprocess
-from Bio import SeqIO
 from Bio import SeqRecord
-from prody import MSA, Sequence, MSAFile,parseMSA
-
+from prody import Polymer
+from Bio.Seq import Seq
+from Bio import AlignIO
+from io import StringIO
+import prody
 from api.ribctl.etl.ribosome_assets import RibosomeAssets
+from api.ribctl.lib.types.types_ribosome import RNA, PolymericFactor, Protein, ProteinClass
 from api.ribctl.msa.msalib import msa_class_proteovision_path, seq_to_fasta
 
 
-rcsb_id = "5AFI"
-p = RibosomeAssets("5AFI").get_chain_by_polymer_class("bL25")
 
-
-bl25 = p.entity_poly_seq_one_letter_code_can
-seq_to_fasta('5afi', bl25, '5afi_bl25.fasta')
+def get_fasta_string(chain:RNA|Protein| PolymericFactor)->str:
+    fasta_description      = "[{}.{}] {} |{}| {}".format(chain.parent_rcsb_id,chain.auth_asym_id, chain.src_organism_names[0], "",  chain.src_organism_ids[0])
+    _seq                   = chain.entity_poly_seq_one_letter_code_can.replace("\n","")
+    seq_record             = SeqRecord.SeqRecord(Seq(_seq).upper())
+    seq_record.id          = fasta_description
+    seq_record.description = ""
+    return seq_record.format('fasta')
 
 
 # ./api/ribctl/muscle3.8 -profile -in1 ./api/ribctl/assets/protein_classes_msa_proteovision/LSU/bL25_ribovision.fasta -in2 5afi_bl25.fasta -quiet
-class_profile = msa_class_proteovision_path('bL25')
-print(class_profile)
-target_seq = '/home/rxz/dev/docker_ribxz/5afi_bl25.fasta'
+# target_seq = '/home/rxz/dev/docker_ribxz/5afi_bl25.fasta'
+# print(class_profile)
 
-from Bio.Seq import Seq
-_seq          = bl25.replace("\n", "")
-seq_record    = SeqRecord.SeqRecord(Seq(_seq).upper())
-seq_record.id = seq_record.description = rcsb_id
-bl25 = seq_record.format('fasta')
 
-cmd = [
-    '/home/rxz/dev/docker_ribxz/api/ribctl/muscle3.8',
-    '-profile',
-    '-in1',
-    class_profile,
-    '-in2',
-    # target_seq,
-    '-',
-    '-quiet']
+rcsb_id    :str         = "5AFI"
+poly_class:ProteinClass = "bL25"
 
-process = subprocess.Popen(cmd,
-                           stdout=subprocess.PIPE,
-                           stdin=subprocess.PIPE,
-                           stderr=subprocess.PIPE, env=os.environ.copy())
+def prot_class_msa_extend(rcsb_id:str, poly_class:ProteinClass)->AlignIO.MultipleSeqAlignment:
+    R                 = RibosomeAssets(rcsb_id)
+    chain             = R.get_chain_by_polymer_class(poly_class)
+    if chain is None:
+        raise LookupError("Could not find chain in {} for protein class: {}".format(rcsb_id,poly_class))
 
-stdout, stderr = process.communicate(input=bl25.encode())
+    fasta_target  = get_fasta_string(chain)
+    class_profile = msa_class_proteovision_path(poly_class)
 
-print(stdout.decode())
-print(stderr.decode())
+    cmd = [
+        '/home/rxz/dev/docker_ribxz/api/ribctl/muscle3.8',
+        '-profile',
+        '-in1',
+        class_profile,
+        '-in2',
+        '-',
+        '-quiet']
 
-# process.wait()
-# print(stdout)
+    process = subprocess.Popen(cmd,
+                               stdout=subprocess.PIPE,
+                               stdin=subprocess.PIPE,
+                               stderr=subprocess.PIPE, env=os.environ.copy())
+
+    stdout, stderr = process.communicate(input=fasta_target.encode())
+    out,err = stdout.decode(),stderr.decode()
+    process.wait()
+
+    msa_file = StringIO(out)
+    msa      = AlignIO.read(msa_file, "fasta")
+
+    return msa
+msa = prot_class_msa_extend(rcsb_id,poly_class)
+
+for s in msa:
+    print(s.seq)
