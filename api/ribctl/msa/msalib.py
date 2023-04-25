8f9d2c6740b3e63f333a475f8ea3b163dbf15019
Artem <rtkushner@gmail.com>
2023-04-24 17:00:36 -0700

    moved methods to lib

--- api/ribctl/msa/msalib.py
+++ api/ribctl/msa/msalib.py
@@ -1,10 +1,12 @@
+from io import StringIO
 import os
 from pprint import pprint
 import typing
 import requests
 from api.ribctl.etl.ribosome_assets import RibosomeAssets
 from api.ribctl.lib.utils import open_structure
-from ribctl.lib.types.types_ribosome import ProteinClass
+from ribctl.lib.types.types_ribosome import RNA, PolymericFactor, Protein, ProteinClass
+from Bio import AlignIO
 from  api.ribctl.lib.types.types_poly_nonpoly_ligand import  list_LSU_Proteins,list_SSU_Proteins
 import argparse
 import subprocess
@@ -95,12 +97,46 @@ def seq_to_fasta(rcsb_id: str, _seq: str, outfile: str):
 
 show = True
 
-"""append target sequence to proteovision msa profile"""
-def muscle_combine_profile(msa_path1: str, msa_path2: str, out_filepath: str):
-    """Combine two MSA-profiles into a single one. Used here to "append" a target sequence two the ribovision alignment. """
-    cmd = ['/home/rxz/dev/docker_ribxz/cli/scripts/muscle3.8', '-profile','-in1', msa_path1, '-in2', msa_path2, '-out', out_filepath]
-    subprocess.Popen(cmd, stdout=subprocess.PIPE,stderr=subprocess.PIPE, env=os.environ.copy()).wait()
-    sys.stdout.flush()
+
+def get_fasta_string(chain:RNA|Protein| PolymericFactor)->str:
+    fasta_description      = "[{}.{}] {} |{}| {}".format(chain.parent_rcsb_id,chain.auth_asym_id, chain.src_organism_names[0], "",  chain.src_organism_ids[0])
+    _seq                   = chain.entity_poly_seq_one_letter_code_can.replace("\n","")
+    seq_record             = SeqRecord.SeqRecord(Seq(_seq).upper())
+    seq_record.id          = fasta_description
+    seq_record.description = ""
+    return seq_record.format('fasta')
+
+def prot_class_msa_extend(rcsb_id:str, poly_class:ProteinClass)->AlignIO.MultipleSeqAlignment:
+    R                 = RibosomeAssets(rcsb_id)
+    chain             = R.get_chain_by_polymer_class(poly_class)
+    if chain is None:
+        raise LookupError("Could not find chain in {} for protein class: {}".format(rcsb_id,poly_class))
+
+    fasta_target  = get_fasta_string(chain)
+    class_profile = msa_class_proteovision_path(poly_class)
+
+    cmd = [
+        '/home/rxz/dev/docker_ribxz/api/ribctl/muscle3.8',
+        '-profile',
+        '-in1',
+        class_profile,
+        '-in2',
+        '-',
+        '-quiet']
+
+    process = subprocess.Popen(cmd,
+                               stdout=subprocess.PIPE,
+                               stdin=subprocess.PIPE,
+                               stderr=subprocess.PIPE, env=os.environ.copy())
+
+    stdout, stderr = process.communicate(input=fasta_target.encode())
+    out,err = stdout.decode(),stderr.decode()
+    process.wait()
+
+    msa_file = StringIO(out)
+    msa      = AlignIO.read(msa_file, "fasta")
+
+    return msa
 
 def msa_class_proteovision_path(class_:ProteinClass):
     def infer_subunit(protein_class:ProteinClass):
