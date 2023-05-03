1306e73d3bd51f2ca65e66e3153d239b4eb819b7
Artem <rtkushner@gmail.com>
2023-05-02 19:36:22 -0700

    cleaving msa down on phylo nbhds

--- api/scratch_taxonomy_workflow.py
+++ api/scratch_taxonomy_workflow.py
@@ -1,17 +1,23 @@
 import os
+from pprint import pprint
 from prody import MSA
 from api.ribctl.lib.types.types_ribosome import ProteinClass
 from api.ribctl.msa.msalib import msa_profiles_dict_prd
 from ete3 import NCBITaxa
 RIBETL_DATA = os.environ.get('RIBETL_DATA')
-rcsb_id     = '3J7Z'
-
+rcsb_id = '3J7Z'
 ncbi = NCBITaxa()
-def get_fasta_taxid(label:str):
+
+msa_profiles: dict[ProteinClass, MSA] = msa_profiles_dict_prd()
+
+
+def get_fasta_taxid(label: str):
     return label.split('|')[-1]
+
 
 def node_lineage(node):
     return ncbi.get_lineage(node.taxid)
+
 
 def lift_rank(taxid: int) -> int:
     """Given a taxid, make sure that it's a SPECIES (as opposed to strain, subspecies, isolate, norank etc.)"""
@@ -24,44 +30,60 @@ def lift_rank(taxid: int) -> int:
             node = next(lin)
         return node
 
-msa_profiles: dict[ProteinClass, MSA] = msa_profiles_dict_prd()
+
+def msa_pick_taxa(msa: MSA, taxids: list[str]):
+    """Given a MSA and a list of taxids, return a new MSA with only the sequences that match the taxids."""
+    seqlabel_tups = [(s, s.getLabel())
+                     for s in msa if get_fasta_taxid(s.getLabel()) in taxids]
+    seqs = [tup[0] for tup in seqlabel_tups]
+    labels = [tup[1] for tup in seqlabel_tups]
+
+    return MSA(seqs, labels=labels)
+
+
+def phylogenetic_neighborhood(taxids_base: list[str], taxid_target: str, n_neighbors: int = 10):
+    tree            = ncbi.get_topology([*taxids_base, taxid_target])
+    target_node     = tree.search_nodes(name=taxid_target)[0]
+    phylo_all_nodes = [
+        (node.name, tree.get_distance(target_node, node))
+        for node in tree.traverse()]
+
+    phylo_extant_nodes = filter(
+        lambda taxid: taxid[0] in taxids_base, phylo_all_nodes)
+    phylo_sorted_nodes = sorted(phylo_extant_nodes, key=lambda x: x[1])
+    nbr_taxids = list(
+        map(lambda tax_phydist: tax_phydist[0], phylo_sorted_nodes))
+
+    # the first element is the target node
+    if len(nbr_taxids) < n_neighbors:
+        return nbr_taxids[1:] 
+    else:
+        return nbr_taxids[1:n_neighbors+1]
+
+
 list_of_taxids = []
 bl12 = msa_profiles['bL12']
 class_taxids = [get_fasta_taxid(p.getLabel()) for p in bl12]
 
-def msa_pick_taxa(msa: MSA, taxids: list[str]):
-    """Given a MSA and a list of taxids, return a new MSA with only the sequences that match the taxids."""
-    return MSA([s for s in msa if get_fasta_taxid(s.getLabel()) in taxids])
 
-def phylogenetic_neighborhood(taxids_base:list[str], taxid_target :str, n_neighbors:int=10):
-    tree        = ncbi.get_topology([*taxids_base, taxid_target])
-    target_node = tree.search_nodes(name=taxid_target)[0]
-
-    phylo_distances   = [(node.name, tree.get_distance(target_node, node)) for node in tree.traverse()]
-    phylo_distances_s = sorted(phylo_distances, key=lambda x: x[1])
-    nbr_taxids        = list(map(lambda tax_phydist: tax_phydist[0], phylo_distances_s))
-
-    if len(nbr_taxids) < n_neighbors:
-        return nbr_taxids
-    else:
-        return nbr_taxids[:n_neighbors]
-
-
-# ex.  418699
-
-class_taxids = phylogenetic_neighborhood(class_taxids, '418699', 4)
-bl12_picked =  msa_pick_taxa(bl12, class_taxids)
+neigbors = phylogenetic_neighborhood(class_taxids, '418699', 4)
+print(neigbors)
+bl12_picked = msa_pick_taxa(bl12, neigbors)
 print(bl12)
 for m in bl12[:4]:
-    print(m)
+    # print(m)
     print(m.getLabel())
+# ['418699', '206389', '28216', '259537']
 
-print("--------")
+print("************************************************************************")
+print("************************************************************************")
+print("************************************************************************")
+print("************************************************************************")
 for m in bl12_picked:
-    print(m)
+    # print(m)
     print(m.getLabel())
 
-#TODO: Ignore intermediate taxa (induct the species, strains, subspecies etc. as nodes)
+# TODO: Ignore intermediate taxa (induct the species, strains, subspecies etc. as nodes)
 # - orgnaism
 # Get the taxonomic IDs of all the taxa in the database
 # all_taxa =ncbi.get_descendant_taxa(1)
