0d2d11f94ab883a3dadba92dbe04b16487fe23f9
Artem <rtkushner@gmail.com>
2023-05-05 02:12:14 -0700

    need raii for peripheras

--- api/scratch_nomenclature.v2.py
+++ api/scratch_nomenclature.v2.py
@@ -13,8 +13,10 @@ from api.rbxz_bend.settings import RIBETL_DATA
 from api.ribctl.etl.ribosome_assets import RibosomeAssets
 from api.ribctl.lib.types.types_ribosome import RNA, Polymer, Protein, ProteinClass
 from api.ribctl.lib.msalib import msa_profiles_dict, msa_profiles_dict_prd, msaclass_extend_process_sub
+from api.taxonomy import filter_by_parent_tax
 
 msa_profiles: dict[ProteinClass, MSA] = msa_profiles_dict_prd()
+
 
 def seq_H_fit_class(base_class: ProteinClass, base_class_msa: MSA, new_seq: str) -> dict[ProteinClass, float]:
     """Calculate entropy difference for a given protein class MSA without and with a new sequence. Used as a measure of fit."""
@@ -25,12 +27,14 @@ def seq_H_fit_class(base_class: ProteinClass, base_class_msa: MSA, new_seq: str)
     H_delta = H_extended - H_original
     return {base_class: H_delta}
 
-async def seq_H_fit_class_multi(chain:Protein, msa_profiles: dict[ProteinClass, MSA], workers=None) -> tuple[ProteinClass, Protein]:
+
+async def seq_H_fit_class_multi(chain: Protein, msa_profiles: dict[ProteinClass, MSA], workers=None) -> tuple[ProteinClass, Protein]:
     print("Processing chain {}...".format(chain.auth_asym_id))
     with ThreadPoolExecutor(max_workers=multiprocessing.cpu_count() if workers is None else workers) as executor:
         futures = []
         for class_name, base_msa in msa_profiles.items():
-            fut = executor.submit(seq_H_fit_class, class_name, base_msa, chain.entity_poly_seq_one_letter_code_can)
+            fut = executor.submit(
+                seq_H_fit_class, class_name, base_msa, chain.entity_poly_seq_one_letter_code_can)
             futures.append(fut)
 
     wait(futures, return_when=ALL_COMPLETED)
@@ -45,16 +49,19 @@ async def seq_H_fit_class_multi(chain:Protein, msa_profiles: dict[ProteinClass, 
     max_fit = min(H_fit, key=H_fit.get)
     return max_fit, chain
 
+
 def classify_chain(chain: Protein, vvv=False):
     return seq_H_fit_class_multi(chain, msa_profiles)
 
-async def struct_classify_chains(chains: list[Protein],vvv=False):
-    return await asyncio.gather(*[seq_H_fit_class_multi(chain, msa_profiles)  for chain in chains] )
 
-def process_struct(rcsb_id:str):
-    R       = RibosomeAssets(rcsb_id.upper())
+async def struct_classify_chains(chains: list[Protein], vvv=False):
+    return await asyncio.gather(*[seq_H_fit_class_multi(chain, msa_profiles) for chain in chains])
+
+
+def process_struct(rcsb_id: str):
+    R = RibosomeAssets(rcsb_id.upper())
     profile = R.profile()
-    rps     = profile.proteins
+    rps = profile.proteins
 
     if len(profile.assembly_map) > 1:
         first_assembly = []
@@ -62,42 +69,45 @@ def process_struct(rcsb_id:str):
             first_assembly.append(
                 chain_inst.rcsb_polymer_entity_instance_container_identifiers.auth_asym_id
             )
-        rps = list(filter(lambda _ : _.auth_asym_id in first_assembly, rps))
+        rps = list(filter(lambda _: _.auth_asym_id in first_assembly, rps))
 
-    loop   = asyncio.get_event_loop()
+    loop = asyncio.get_event_loop()
     result = loop.run_until_complete(struct_classify_chains(rps, vvv=True))
-    total  = {}
-    for assigned_class,chain in result:
-        total.update({chain.auth_asym_id : assigned_class})
+    total = {}
+    for assigned_class, chain in result:
+        total.update({chain.auth_asym_id: assigned_class})
     return total
 
-    
-    
-    
 
-
-global counter;
+global counter
 counter = 0
 
-assets_nomenclaturev2 = os.path.join("/home/rxz/dev/docker_ribxz/api/ribctl/assets/nomenclaturev2")
+assets_nomenclaturev2 = os.path.join(
+    "/home/rxz/dev/docker_ribxz/api/ribctl/assets/nomenclaturev2")
 
 loop = asyncio.get_event_loop()
 with ProcessPoolExecutor(max_workers=10) as pool:
-    for struct in os.listdir(RIBETL_DATA):
+    for struct in filter_by_parent_tax(2):
+        def nomv2_path(structid): return os.path.join(
+            assets_nomenclaturev2, f"{structid.upper()}.json")
+        if os.path.exists(nomv2_path(struct)):
+            print("Skipping {}".format(struct))
+            continue
+
         if len(struct) != 4 or struct == '6OXI':
             continue
         x = pool.submit(process_struct, struct)
+
         def save_fut_result(rcsb_id):
-            def _(f:Future):
-                global counter 
-                counter +=1
+            def _(f: Future):
+                global counter
+                counter += 1
                 if f.exception() is not None:
                     raise LookupError()
                 else:
                     r = f.result()
-                    with open(os.path.join(assets_nomenclaturev2, f"{rcsb_id.upper()}.json"),'w') as outfile:
+                    with open(nomv2_path(rcsb_id), 'w') as outfile:
                         json.dump(r, outfile, indent=4)
-                    print("Processed structs-------------------------------------->: {}".format(counter))
             return _
 
         x.add_done_callback(save_fut_result(struct))
