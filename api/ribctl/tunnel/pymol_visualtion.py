3323d75d655678d1803aeeba93fea8823eb6e55e
Artem <rtkushner@gmail.com>
2023-05-05 18:00:41 -0700

    some vis tweaks

--- api/ribctl/tunnel/pymol_visualtion.py
+++ api/ribctl/tunnel/pymol_visualtion.py
@@ -7,6 +7,114 @@ from pymol import cmd
 
 RIBETL_DATA = os.environ.get("RIBETL_DATA")
 
+
+colormap__LSU_Proteins = {
+                          "uL1": "lawrencium",
+                          "uL2": "lead",
+                          "uL3": "lithium",
+                          "uL4": "lutetium",
+                          "uL5": "magnesium",
+                          "uL6": "manganese",
+                          "eL6": "meitnerium",
+                          "eL8": "mendelevium",
+                          "bL9": "mercury",
+                          "uL10": "molybdenum",
+                          "uL11": "neodymium",
+                          "bL12": "neon",
+                          "uL13": "neptunium",
+                          "eL13": "nickel",
+                          "uL14": "niobium",
+                          "eL14": "nitrogen",
+                          "uL15": "nobelium",
+                          "eL15": "osmium",
+                          "uL16": "oxygen",
+                          "bL17": "palladium",
+                          "uL18": "phosphorus",
+                          "eL18": "platinum",
+                          "bL19": "plutonium",
+                          "eL19": "polonium",
+                          "bL20": "potassium",
+                          "eL20": "praseodymium",
+                          "bL21": "promethium",
+                          "eL21": "protactinium",
+                          "uL22": "radium",
+                          "eL22": "radon",
+                          "uL23": "rhenium",
+                          "uL24": "rhodium",
+                          "eL24": "rubidium",
+                          "bL25": "ruthenium",
+                          "bL27": "rutherfordium",
+                          "eL27": "samarium",
+                          "bL28": "scandium",
+                          "eL28": "seaborgium",
+                          "uL29": "selenium",
+                          "eL29": "silicon",
+                          "uL30": "silver",
+                          "eL30": "sodium",
+                          "bL31": "strontium",
+                          "eL31": "sulfur",
+                          "bL32": "tantalum",
+                          "eL32": "technetium",
+                          "bL33": "tellurium",
+                          "eL33": "terbium",
+                          "bL34": "thallium",
+                          "eL34": "thorium",
+                          "bL35": "thulium",
+                          "bL36": "tin",
+                          "eL36": "titanium",
+                          "eL37": "tungsten",
+                          "eL38": "uranium",
+                          "eL39": "vanadium",
+                          "eL40": "xenon",
+                          "eL41": "ytterbium",
+                          "eL42": "yttrium",
+                          "eL43": "zinc",
+                          "P1/P2": "zirconium"
+                          
+                          }
+
+colormap__SSU_Proteins = {
+                     "bS1": "actinium",
+                     "eS1": "aluminum",
+                     "uS2": "americium",
+                     "uS3": "antimony",
+                     "uS4": "argon",
+                     "eS4": "arsenic",
+                     "uS5": "astatine",
+                     "bS6": "barium",
+                     "eS6": "berkelium",
+                     "uS7": "beryllium",
+                     "eS7": "bismuth",
+                     "uS8": "bohrium",
+                     "eS8": "boron",
+                     "uS9": "bromine",
+                     "uS10": "cadmium",
+                     "eS10": "calcium",
+                     "uS11": "californium",
+                     "uS12": "carbon",
+                     "eS12": "cerium",
+                     "uS13": "cesium",
+                     "uS14": "chlorine",
+                     "uS15": "chromium",
+                     "bS16": "cobalt",
+                     "uS17": "copper",
+                     "eS17": "curium",
+                     "bS18": "deuterium",
+                     "uS19": "dubnium",
+                     "eS19": "dysprosium",
+                     "bS20": "einsteinium",
+                     "bS21": "erbium",
+                     "bTHX": "europium",
+                     "eS21": "fermium",
+                     "eS24": "fluorine",
+                     "eS25": "francium",
+                     "eS26": "gadolinium",
+                     "eS27": "gallium",
+                     "eS28": "germanium",
+                     "eS30": "gold",
+                     "eS31": "hafnium",
+                     }
+
 def build_selection_string( chain_name: str, res_ids: List[int]):
     """pymol command being issued"""
     selection_name    = '23-28S_{}'.format(chain_name)
@@ -85,13 +193,14 @@ def visualize_obstructions(rcsb_id):
         cmd.remove(ressele)
         cmd.color("blue", resname)
 
-# def struct_paint_chains(rcsb_id):
-
 def struct_paint_chains(pdbid: str):
     pdbid          = pdbid.upper()
     RIBETL_DATA    = str(os.environ.get('RIBETL_DATA'))
     nomenclaturev2 = os.path.join(RIBETL_DATA, pdbid, f"{pdbid}_nomenclaturev2.json")
     profilepath    = os.path.join(RIBETL_DATA, pdbid, f"{pdbid}.json")
+
+    with open(nomenclaturev2, 'rb') as ninfile:
+        nomenclaturev2 = json.load(ninfile)
 
     with open(profilepath, 'rb') as infile:
         profile = json.load(infile)
@@ -105,24 +214,24 @@ def struct_paint_chains(pdbid: str):
             cmd.set('transparency', 0.1, f"chain {rna['auth_asym_id']}")
 
     for protein in profile['proteins']:
+        nomclass = nomenclaturev2[protein['auth_asym_id']]
 
-        if len(protein['nomenclature']) != 0:
-            if protein['nomenclature'][0] in colormap__LSU_Proteins:
-                CLR = colormap__LSU_Proteins[protein['nomenclature'][0]]
-            elif protein['nomenclature'][0] in colormap__SSU_Proteins:
-                CLR = colormap__SSU_Proteins[protein['nomenclature'][0]]
-        else:
-            CLR = 'blue'
+        if nomclass in colormap__LSU_Proteins:
+            CLR = colormap__LSU_Proteins[nomclass]
+        elif nomclass in colormap__SSU_Proteins:
+            CLR = colormap__SSU_Proteins[nomclass]
 
-        prot_tmp = f"protein_tmp_{ protein['auth_asym_id'] }"
+        prot_tmp = f"{nomclass}.{protein['auth_asym_id']}"
 
         cmd.create(prot_tmp, f"chain {protein[ 'auth_asym_id' ]}")
+
         cmd.remove(f"chain {protein[ 'auth_asym_id' ]} and m. {pdbid}")
-        cmd.hide ('everything', prot_tmp)
-        cmd.show ('surface', prot_tmp)
-        cmd.show ('sticks', prot_tmp)
-        cmd.show ('cartoon', prot_tmp)
-        cmd.show ('lines', prot_tmp)
+
+        cmd.hide('everything', prot_tmp)
+        cmd.show('surface', prot_tmp)
+        cmd.show('sticks', prot_tmp)
+        cmd.show('cartoon', prot_tmp)
+        cmd.show('lines', prot_tmp)
         cmd.set('transparency', 0.75, prot_tmp)
         cmd.color(CLR , prot_tmp)
 
@@ -134,7 +243,7 @@ def sload(pdbid: str):
     cmd.load(path)
 
 cmd.extend("sload", sload)
-cmd.extend("by_rna", struct_paint_chains)
+cmd.extend("by_chain", struct_paint_chains)
 cmd.extend("ptc", ptc)
 cmd.extend("ptc_w_markers", ptc_raw_w_markerks)
 cmd.extend("list_bacteria", list_bacteria)
