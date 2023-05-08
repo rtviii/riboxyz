bed48ddd41a411bd97f8d4c883469c5a8912c8a8
Artem <rtkushner@gmail.com>
2023-05-07 20:24:14 -0700

    make sure to inject hash(randbytes(x)) into tempfile names, contention otherwise

--- api/scratch_nomenclature.v2.py
+++ api/scratch_nomenclature.v2.py
@@ -8,27 +8,30 @@ from pprint import pprint
 from prody import MSA, calcShannonEntropy
 from api.rbxz_bend.settings import RIBETL_DATA
 from api.ribctl.etl.ribosome_assets import RibosomeAssets
-from api.ribctl.lib.types.types_ribosome import  Protein, ProteinClass
-from api.ribctl.lib.msalib import  msadict_get_meta_info, msa_profiles_dict_prd, msaclass_extend, msaclass_extend_temp
+from api.ribctl.lib.types.types_ribosome import Protein, ProteinClass
+from api.ribctl.lib.msalib import msadict_get_meta_info, msa_profiles_dict_prd, msaclass_extend, msaclass_extend_temp
 from api.ribctl.taxonomy import filter_by_parent_tax
 
 msa_profiles: dict[ProteinClass, MSA] = msa_profiles_dict_prd()
 
-def seq_H_fit_class(base_class: ProteinClass, base_class_msa: MSA, target_fasta: str, target_auth_asym_id:str, parent_rcsb_id:str) -> dict[ProteinClass, float]:
-    """Calculate entropy difference for a given protein class MSA without and with a new sequence. Used as a measure of fit."""
-    extended_class = msaclass_extend_temp(base_class, base_class_msa, target_fasta, target_auth_asym_id, parent_rcsb_id)
-    H_original     = sum(calcShannonEntropy(base_class_msa))
-    H_extended     = sum(calcShannonEntropy(extended_class))
-    H_delta        = H_extended - H_original
-    # pprint("{} class has {} sequences".format(base_class,base_class_msa.numSequences()))
 
+def seq_H_fit_class(base_class: ProteinClass, base_class_msa: MSA, target_fasta: str, target_auth_asym_id: str, parent_rcsb_id: str) -> dict[ProteinClass, float]:
+    """Calculate entropy difference for a given protein class MSA without and with a new sequence. Used as a measure of fit."""
+    extended_class = msaclass_extend_temp(
+        base_class, base_class_msa, target_fasta, target_auth_asym_id, parent_rcsb_id)
+    H_original = sum(calcShannonEntropy(base_class_msa))
+    H_extended = sum(calcShannonEntropy(extended_class))
+    H_delta = H_extended - H_original
+    # pprint("{} class has {} sequences".format(base_class,base_class_msa.numSequences()))
     return {base_class: H_delta}
 
 async def seq_H_fit_class_multi(chain: Protein, msa_profiles: dict[ProteinClass, MSA], workers=None) -> tuple[ProteinClass, Protein]:
     with ThreadPoolExecutor(max_workers=multiprocessing.cpu_count() if workers is None else workers) as executor:
         futures = []
+
         for class_name, base_msa in msa_profiles.items():
-            fut = executor.submit(seq_H_fit_class, class_name, base_msa, chain.entity_poly_seq_one_letter_code_can, chain.auth_asym_id, chain.parent_rcsb_id)
+            fut = executor.submit(seq_H_fit_class, class_name, base_msa,
+                                  chain.entity_poly_seq_one_letter_code_can, chain.auth_asym_id, chain.parent_rcsb_id)
             futures.append(fut)
 
     wait(futures, return_when=ALL_COMPLETED)
@@ -41,17 +44,19 @@ async def seq_H_fit_class_multi(chain: Protein, msa_profiles: dict[ProteinClass,
         else:
             H_fit.update(f.result())
 
-
     max_fit = min(H_fit, key=H_fit.get)
     # pprint(H_fit)
     print("Picked class {}({} sequences) with fit {}".format(max_fit, H_fit[max_fit]))
     return max_fit, chain
 
+
 def classify_chain(chain: Protein, vvv=False):
     return seq_H_fit_class_multi(chain, msa_profiles)
 
+
 async def struct_classify_chains(chains: list[Protein], vvv=False):
-    return await asyncio.gather(*[seq_H_fit_class_multi(chain, msa_profiles) for chain in chains])
+    return await asyncio.gather(*[seq_H_fit_class_multi(chain, msa_profiles) for chain in chains[:5]]) # * <--------
+
 
 def process_struct(rcsb_id: str):
     R       = RibosomeAssets(rcsb_id.upper())
@@ -62,14 +67,14 @@ def process_struct(rcsb_id: str):
     if len(profile.assembly_map) > 1:
         first_assembly = []
         for chain_inst in profile.assembly_map[0].polymer_entity_instances:
-            first_assembly.append(chain_inst.rcsb_polymer_entity_instance_container_identifiers.auth_asym_id)
+            first_assembly.append(
+                chain_inst.rcsb_polymer_entity_instance_container_identifiers.auth_asym_id)
         rps = list(filter(lambda _: _.auth_asym_id in first_assembly, rps))
 
-
-    loop   = asyncio.get_event_loop()
+    loop = asyncio.get_event_loop()
     result = loop.run_until_complete(struct_classify_chains(rps, vvv=True))
 
-    total  = {}
+    total = {}
     for assigned_class, chain in result:
         total.update({chain.auth_asym_id: assigned_class})
 
@@ -82,18 +87,21 @@ def nomv2_duplicates():
             prof = json.load(f)
 
         classes = list(prof.values())
-        dup     = {x for x in classes if classes.count(x) > 1}
+        dup = {x for x in classes if classes.count(x) > 1}
 
         print(rcsb_id, dup)
+
 
 def ____generate_nomenclature():
     global counter
     counter = 0
-    assets_nomenclaturev2 = os.path.join("/home/rxz/dev/docker_ribxz/api/ribctl/assets/nomenclaturev2")
+    assets_nomenclaturev2 = os.path.join(
+        "/home/rxz/dev/docker_ribxz/api/ribctl/assets/nomenclaturev2")
     loop = asyncio.get_event_loop()
+
     with ProcessPoolExecutor(max_workers=10) as pool:
         # for struct in filter_by_parent_tax(2):
-        for struct in ['7OF4']:
+        for struct in ['3J7Z']:
 
             def nomv2_path(structid): return os.path.join(
                 assets_nomenclaturev2, f"{structid.upper()}.json")
@@ -111,6 +119,7 @@ def ____generate_nomenclature():
                     global counter
                     counter += 1
                     if f.exception() is not None:
+                        print(f.exception())
                         raise LookupError()
                     else:
                         r = f.result()
@@ -122,10 +131,10 @@ def ____generate_nomenclature():
 
 
 if __name__ == "__main__":
-    ____generate_nomenclature()
+    process_struct('3J7Z')
 
 
-#-------------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------
 # loop.run_until_complete(struct_classify_chains(R.profile().proteins, loop, vvv=True))
 # with ProcessPoolExecutor(max_workers=10) as pool:
 #     coroutines = []
@@ -175,7 +184,6 @@ if __name__ == "__main__":
     #         if len(rna.nomenclature) != 0:
     #             total[rcsb_id]['classified'] += 1
     # return total
-
 
 
 # all      = d.items()
