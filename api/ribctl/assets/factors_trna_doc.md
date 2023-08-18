a1a506756922600d4a7061bbd3a1416f63a3c506
Artem <rtkushner@gmail.com>
2023-08-18 13:20:41 -0700

    trna/factor table

---
+++ api/ribctl/assets/factors_trna_doc.md
@@ -0,0 +1,57 @@
+
+## General categories of factors associated with the ribosome's lifecycle
+
+|   	|  Eukaryotic 	| Bacteria  	|   	|   	|
+|:---:	|:---:	|:---:	|---	|---	|
+|                          **Assembly factors**  	
+|   	| RbfA (Ribosome Binding Factor A)          |  RbfA (Ribosome Binding Factor A) 	|   	|   	|
+|   	| RimM (Ribosome Maturation Factor M)   	|  RimM (Ribosome Maturation Factor M) 	|   	|   	|
+|   	|   	|  Era (Era GTPase) 	|   	|   	|
+|   	|                                         	|  EngA (GTPase EngA) 	|   	|   	|
+|   	|                                         	|  ObgE (Obg GTPase) 	|   	|   	|
+|**Initiation factors**  	|   	|   	|   	|   	|
+|   	|eIF1 (Eukaryotic Initiation Factor 1)   	|  IF1 (Initiation Factor 1) 	|   	|   	|
+|   	|eIF1A (Eukaryotic Initiation Factor 1A)   	|  IF2 (Initiation Factor 2) 	|   	|   	|
+|   	|eIF2 (Eukaryotic Initiation Factor 2)   	|  IF3 (Initiation Factor 3) 	|   	|   	|
+|   	|eIF3 (Eukaryotic Initiation Factor 3)   	|   	|   	|   	|
+|   	|eIF4 (Eukaryotic Initiation Factor 4)   	|   	|   	|   	|
+|   	|eIF5B (Eukaryotic Initiation Factor 5B)   	|   	|   	|   	|
+|   	|eIF6 (Eukaryotic Initiation Factor 6)   	|   	|   	|   	|
+|**Release factors**  	|   	|   	|   	|   	|
+|   	|  eRF1 (Eukaryotic Release Factor 1) 	|RF1 (Release Factor 1)   	|   	|   	|
+|   	|  eRF3 (Eukaryotic Release Factor 3) 	|RF2 (Release Factor 2)   	|   	|   	|
+|   	|   	                                |RF3 (Release Factor 3)   	|   	|   	|
+|**Recycling factors**  	|   	|   	|   	|   	|
+|   	|   ABCE1 (ATP-Binding Cassette E1)	|RRF (Ribosome Recycling Factor)   	|   	|   	|
+|   	|   	                            |RliB (Ribosome Large Subunit Interface B)   	|   	|   	|
+|**RNA Helicase**   	|   	|   	|   	|   	|
+|   	|   	| RhlB (RNA Helicase B)  	|   	|   	|
+|   	|                                       	| DbpA (ATP-dependent RNA Helicase DbpA)  	|   	|   	|
+|   	|                                       	| DbpB (ATP-dependent RNA Helicase DbpB)  	|   	|   	|
+|   	|                                       	| Prp43 (Pre-mRNA Processing Factor 43)  	|   	|   	|
+|**Extra-ribosomal factors**   	|   	|   	|   	|   	|
+|   	|   	|HflX (ATPase HflX)   	|   	|   	|
+|   	|   	                                    |YjeQ (Obg GTPase)   	|   	|   	|
+|   	|   	                                    |YihA (Bacterial Stress Protein)   	|   	|   	|
+|**Quality control factors**   	|   	|   	|   	|   	|
+|   	| Pelota (Homolog of Bacterial Trans-Translation System)  	|   	|   	|   	|
+|   	| Hbs1 (Homolog of Bacterial Trans-Translation System)  	|   	|   	|   	|
+|**Maturation factors**   	|   	|   	|   	|   	|
+|   	| Rea1 (Ribosome Export Associated 1)  	|tmRNA (Transfer-messenger RNA)   	|   	|   	|
+|   	|   	                                |ArfA (Alternative Ribosome-Rescue Factor A)   	|   	|   	|
+|   	|   	                                |ArfB (Alternative Ribosome-Rescue Factor B)   	|   	|   	|
+| **Trans-translocation factors**   	||   	|   	|   	|   	|
+|   	|   -	| SmpB (Small Protein B)|   	|   	|
+|   	|   -	| tmRNA (Transfer-messenger RNA)  	|   	|   	|
+
+
+## General categories of tRNA  associated with the ribosome's lifecycle
+
+- tRNAfMet
+- tRNAiMet
+- tRNAHis
+- Ter-tRNA1 
+- Ter-tRNA2 
+- Trans-Translation(?) tRNA
+- Elongation: AA-specific tRNAs
+
---
+++ ribctl/lib/types/types_ribosome.py
@@ -0,0 +1,158 @@
+from typing import Any, Optional
+import typing
+from typing import NewType
+from pydantic import BaseModel
+from ribctl.lib.types.types_poly_nonpoly_ligand import LSUProteinClass, NonpolymericLigandClass, PolymericFactorClass, RNAClass, SSUProteinClass
+
+RCSB_ID = NewType('RCSB_ID', str)
+
+ProteinClass = typing.Union[LSUProteinClass , SSUProteinClass]
+PolymerClass = typing.Union[ProteinClass, RNAClass]
+
+class Polymer(BaseModel):
+    def __hash__(self):
+        return hash(self.auth_asym_id + self.parent_rcsb_id)
+
+    assembly_id: int
+
+    asym_ids: list[str]
+    auth_asym_id: str
+
+    parent_rcsb_id   : str
+
+    src_organism_names : list[str]
+    host_organism_names: list[str]
+
+    src_organism_ids   : list[int]
+    host_organism_ids  : list[int]
+
+    rcsb_pdbx_description: str | None
+
+    entity_poly_strand_id              : str
+    entity_poly_seq_one_letter_code    : str
+    entity_poly_seq_one_letter_code_can: str
+    entity_poly_seq_length             : int
+    entity_poly_polymer_type           : str
+    entity_poly_entity_type            : str
+
+    nomenclature:  list[PolymerClass]
+
+class Protein(Polymer):
+
+    def __hash__(self):
+        return hash(self.auth_asym_id + self.parent_rcsb_id)
+
+    pfam_accessions  : list[str]
+    pfam_comments    : list[str]
+    pfam_descriptions: list[str]
+
+    uniprot_accession: list[str]
+
+    def to_poly(self)->Polymer:
+        return Polymer(**self.dict())
+
+class RNA(Polymer):
+    def __hash__(self):
+        return hash(self.auth_asym_id + self.parent_rcsb_id)
+
+    pass
+
+class NonpolymericLigand(BaseModel)  : 
+
+      chemicalId         : str
+      chemicalName       : str
+      formula_weight     : None | float
+      pdbx_description   : str
+      number_of_instances: int
+    #   nomenclature       : list[NonpolymericLigandClass]
+
+class PolymericFactor(Polymer): 
+    def __hash__(self) -> int:
+        return hash(self.auth_asym_id + self.parent_rcsb_id)
+
+    nomenclature: list[PolymericFactorClass] 
+    
+class NonpolymerEntityInstance(BaseModel):
+    class NonpolymerEntityInstanceContainerIdentifiers(BaseModel):
+        entity_id: str
+        auth_asym_id: str
+        auth_seq_id: str
+
+    rcsb_nonpolymer_entity_instance_container_identifiers: NonpolymerEntityInstanceContainerIdentifiers
+
+class PolymerEntityInstance(BaseModel):
+    class PolymerEntityInstanceContainerIdentifiers(BaseModel):
+        entity_id: str
+        auth_asym_id: str
+    rcsb_polymer_entity_instance_container_identifiers: PolymerEntityInstanceContainerIdentifiers
+
+class AssemblyInstancesMap(BaseModel):
+    """
+    This basically specifies which assembly an instnace of a polymer or a nonpolymer belongs to. 
+    Certain PDB structures come with more than a single physical model/assembly packaged in the file,
+    hence every chain and many ligands might be present in 2 or more instances. 
+    
+    The RNA/Protein/Ligand suffices to characterizes all instances, yet, to resolve duplicate chains 
+    precisely in space, this information is needed.
+    
+    assemblies{
+    rcsb_id 
+   	nonpolymer_entity_instances{
+  
+      rcsb_nonpolymer_entity_instance_container_identifiers{
+        entity_id
+        comp_id
+        auth_asym_id
+        rcsb_id
+        auth_seq_id
+      }
+    }
+    polymer_entity_instances{
+       rcsb_polymer_entity_instance_container_identifiers {  
+        entity_id
+        asym_id
+        auth_asym_id
+        entry_id
+        entity_id
+      }
+    }
+  }
+    """
+    rcsb_id                    : str # 5AFI-1
+    nonpolymer_entity_instances: Optional[list[NonpolymerEntityInstance]]
+    polymer_entity_instances   : list[PolymerEntityInstance]
+
+class RibosomeStructure(BaseModel):
+
+    rcsb_id   : str
+    expMethod : str
+    resolution: float
+
+    pdbx_keywords:      str | None
+    pdbx_keywords_text: str | None
+
+    rcsb_external_ref_id  : list[str]
+    rcsb_external_ref_type: list[str]
+    rcsb_external_ref_link: list[str]
+
+    citation_year        : None | int
+    citation_rcsb_authors: None | list[str]
+    citation_title       : None | str
+    citation_pdbx_doi    : None | str
+
+    src_organism_ids  : list[int]
+    src_organism_names: list[str]
+
+    host_organism_ids  : list[int]
+    host_organism_names: list[str]
+
+    assembly_map: list[AssemblyInstancesMap]
+
+    proteins            : list[Protein]
+    rnas                : list[RNA] | None
+    nonpolymeric_ligands: list[NonpolymericLigand] | None
+    polymeric_factors   : list[PolymericFactor] | None
+    
+    @staticmethod
+    def from_json_profile(d: Any):
+        return RibosomeStructure(**d)
