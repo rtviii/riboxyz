720c2836068989670e1a06ddcd98a9e0c67e4e7d
rtviii <rtkushner@gmail.com>
2023-09-16 19:07:54 -0500

    straightening out pathing

--- ribd_cli/etl.py
+++ ribd_cli/etl.py
@@ -4,38 +4,48 @@ from ribctl import RIBETL_DATA
 from ribctl.ribosome_assets import Assetlist, RibosomeAssets, obtain_assets, obtain_assets_threadpool
 
 def cmd_etl(args):
-    print(args)
-    print("")
-    if args.obtain_all_structures:
-        ASL = Assetlist(profile=True)
 
-        requested_assets =[]
-        if args.tunnel:
-            requested_assets.append('tunnel')
-        if args.cif:
-            asyncio.run(RibosomeAssets(args.rcsb_id)._verify_cif())
-        if args.updated_cif:
-            print("getting updated cif")
-        if args.chains:
-            print("getting chains")
-        
+    ASL = Assetlist(profile=True)
+
+    if args.ptc_coords:
+        ASL.ptc_coords=True
+
+    if args.cif_and_chains:
+        ASL.cif_and_chains=True
+
+    if args.cif_updated:
+        ASL.cif_updated=True
+
+    if args.factors_and_ligands:
+        ASL.factors_and_ligands=True
+
+    if args.png_thumbnail:
+        ASL.png_thumbnail=True
+
+    #All structures
+    if args.obtain_all_structures:
 
         obtain_assets_threadpool(
             [],
             ASL,
             workers=16,
             get_all=True,
-            overwrite=True
+            overwrite=args.overwrte or False
         )
+
+    #Single structure
     if args.rcsb_id:
-        if args.tunnel:
-            print(RibosomeAssets(args.rcsb_id)._ptc_residues())
-        if args.cif:
-            asyncio.run(RibosomeAssets(args.rcsb_id)._verify_cif())
-        if args.updated_cif:
-            print("getting updated cif")
-        if args.chains:
-            print("getting chains")
+        RCSB_ID = str(args.rcsb_id)
+        loop    = asyncio.get_event_loop()
+        loop.run_until_complete(
+            obtain_assets(
+                RCSB_ID,
+                ASL,
+                args.overwrite or False
+            )
+        )
+
+
 
 
 
