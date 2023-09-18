ba7c66f85da0073bace0e63286cead67b944f9ab
rtviii <rtkushner@gmail.com>
2023-09-18 00:50:23 -0500

    elaborating

--- scripts/test_bp.py
+++ scripts/test_bp.py
@@ -1,12 +1,34 @@
 import os
 from pprint import pprint
+from typing import Iterator
 from Bio.Align import MultipleSeqAlignment,Seq, SeqRecord
 from Bio import SeqIO
 import re
 
+from ete3 import NCBITaxa
 from ribctl import ASSETS
 from ribctl.lib.types.types_poly_nonpoly_ligand import list_ProteinClass
 from ribctl.ribosome_assets import RibosomeAssets
+
+
+class Fasta:
+    records = list[SeqRecord]
+
+    def __init__(self, path:str) -> None:
+        try:
+            with open(path, "r") as fasta_in:
+                self.records = [*SeqIO.parse(fasta_in, "fasta")]
+        except FileNotFoundError:
+            print(f"File not found: {path}")
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
+    def pick_taxids(self, taxids: list[str]) -> list[SeqRecord]:
+        return list(filter(lambda record: record.id in taxids, self.records))
+    def all_taxids(self) ->list[int]:
+        taxids = []
+        for record in self.records:
+            taxids =[*taxids, record.id]
+        return taxids
 
 
 def process_fasta(input_file, output_file):
@@ -40,9 +62,29 @@ def process_fasta(input_file, output_file):
 #     process_fasta(fasta_file_path_in, fasta_file_path_out)
 
 
+def phylogenetic_neighborhood(taxids_base: list[str], taxid_target: str, n_neighbors: int = 10) -> list[str]:
+    """Given a set of taxids and a target taxid, return a list of the [n_neighbors] phylogenetically closest to the target."""
+    tree = NCBITaxa().get_topology(list(set([*taxids_base, str(taxid_target)])))
+    target_node = tree.search_nodes(name=str(taxid_target))[0]
+    phylo_all_nodes = [
+        (node.name, tree.get_distance(target_node, node))
+        for node in tree.traverse()]
+
+    phylo_extant_nodes = filter(
+        lambda taxid: taxid[0] in taxids_base, phylo_all_nodes)
+
+    phylo_sorted_nodes = sorted(phylo_extant_nodes, key=lambda x: x[1])
+
+    nbr_taxids = list(
+        map(lambda tax_phydist: tax_phydist[0], phylo_sorted_nodes))
+
+    # the first element is the target node
+    if len(nbr_taxids) < n_neighbors:
+        return nbr_taxids[1:]
+    else:
+        return nbr_taxids[1:n_neighbors+1]
 
 #!% ------------------------------------------------------------------------------------------------------------------------------------------------
-
 
 def fasta_get_taxids(infile:str)->list[int]:
     """Given a fasta file, return all the taxids present in it
@@ -60,19 +102,26 @@ def fasta_get_taxids(infile:str)->list[int]:
     return taxids
 
 
-
-rib = RibosomeAssets('3J7Z').profile()
+rib      = RibosomeAssets('3J7Z').profile()
 organism = rib.src_organism_ids[0]
-prots = RibosomeAssets('3J7Z').profile().proteins
+prots    = RibosomeAssets('3J7Z').profile().proteins
 
 i = 0
 for protclass in list_ProteinClass:
-    ids = fasta_get_taxids(os.path.join(ASSETS["fasta_ribosomal_proteins"], f"{protclass}.fasta"))
-    
+    fasta_path = os.path.join(ASSETS["fasta_ribosomal_proteins"], f"{protclass}.fasta")
+    ids = fasta_get_taxids(fasta_path)
+    phylo_nbhd=  phylogenetic_neighborhood(list(map(lambda x: str(x),ids)), str(organism), n_neighbors=10)
+    Fasta(fasta_path).pick_taxids(phylo_nbhd)
+
     i+=1
     if i > 5:
         exit()
-    # print(protclass)
+
+
+
+fasta_path = os.path.join(ASSETS["fasta_ribosomal_proteins"], f"bL9.fasta")
+record = [*SeqIO.parse(fasta_path, "fasta")]
+
 
 
 
