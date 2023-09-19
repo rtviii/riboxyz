9240820169caea34f2389fafdbf3f60ca48982d6
rtviii <rtkushner@gmail.com>
2023-09-18 22:45:17 -0500

    hmm methods piling up

--- ribctl/lib/classification.py
+++ ribctl/lib/classification.py
@@ -92,16 +92,86 @@ def seq_prot_against_protclasses(seq:str, hmm_dict:dict)->dict[ProteinClass, lis
 # print(prots[14])
 
 def classify_sequence(seq:str, organism:int, candidate_category: ProteinClassEnum | RNAClassEnum  ):
-
     if candidate_category == "ribosomal_protein":
         ...
 
-def hmm_generate(candidate_class: ProteinClassEnum | RNAClassEnum, organism:int):
-    if candidate_class in ProteinClassEnum:
-        print("got protein class")
-    if candidate_class in RNAClassEnum:
-        print("got rna")
+def hmm_create(name:str, seqs:Iterator[SeqRecord], alphabet:Alphabet)->HMM:
+    """Create an HMM from a list of sequences"""
+    seq_tuples =  [TextSequence(name=bytes(seq.id, 'utf-8'), sequence=str(seq.seq)) for seq in seqs]
+    builder       = pyhmmer.plan7.Builder(alphabet)
+    background    = pyhmmer.plan7.Background(alphabet) #? The null(background) model can be later augmented.
+    anonymous_msa = pyhmmer.easel.TextMSA(bytes(name, 'utf-8'),sequences=seq_tuples)
+    HMM, _profile, _optmized_profile = builder.build_msa(anonymous_msa.digitize(alphabet), background)
+    return HMM
 
+def fasta_phylogenetic_correction(candidate_class:ProteinClassEnum|RNAClassEnum, organism_taxid:int, n_neighbors=10)->Iterator[SeqRecord]:
+    """Given a candidate class and an organism taxid, retrieve the corresponding fasta file, and perform phylogenetic correction on it."""
+    if candidate_class in ProteinClassEnum:
+        fasta_path = os.path.join(ASSETS["fasta_ribosomal_proteins"], f"{candidate_class}.fasta")
+    elif candidate_class in RNAClassEnum:
+        fasta_path = os.path.join(ASSETS["fasta_ribosomal_rna"], f"{candidate_class}.fasta")
+    else:
+        raise Exception("Invalid candidate class")
+
+    records      = Fasta(fasta_path)
+    ids          = records.all_taxids()
+    phylo_nbhd   = phylogenetic_neighborhood(list(map(lambda x: str(x),ids)), str(organism_taxid), n_neighbors)
+    seqs         = records.pick_taxids(phylo_nbhd)
+    return iter(seqs)
+
+def hmm_cache(hmm:HMM):
+    name     = hmm.name.decode('utf-8')
+    filename = os.path.join(hmm_cachedir, name)
+
+    if not os.path.isfile(filename):
+        with open(filename, "wb") as hmm_file:
+            hmm.write(hmm_file)
+            print("Wrote `{}` to `{}`".format(filename, hmm_cachedir))
+    else:
+        ...
+        # print(filename + " exists.")
+
+def hmm_produce(candidate_class: ProteinClassEnum | RNAClassEnum, organism_taxid:int)->HMM:
+    """Produce an organism-specific HMM. Retrieve from cache if exists, otherwise generate and cache."""
+    hmm_path = "class_{}_taxid_{}.hmm".format(candidate_class, organism_taxid)
+    if os.path.isfile(os.path.join(hmm_cachedir, hmm_path)):
+        hmm_path = os.path.join(hmm_cachedir, hmm_path)
+        with pyhmmer.plan7.HMMFile(hmm_path) as hmm_file:
+            HMM = hmm_file.read()
+            return HMM
+    else:
+        if candidate_class in ProteinClassEnum:
+
+            # fasta_path   = os.path.join(ASSETS["fasta_ribosomal_proteins"], f"{candidate_class}.fasta")
+            # records      = Fasta(fasta_path)
+            # ids          = records.all_taxids()
+            # phylo_nbhd   = phylogenetic_neighborhood(list(map(lambda x: str(x),ids)), str(organism_taxid), n_neighbors=10)
+            # seqs         = records.pick_taxids(phylo_nbhd)
+            # seqs_aligned = muscle_align_N_seq( iter(seqs))
+
+            
+            # seq_tuples =  [TextSequence(name=bytes(seq.id, 'utf-8'), sequence=str(seq.seq)) for seq in seqs_aligned]
+            # builder       = pyhmmer.plan7.Builder(alphabet)
+            # background    = pyhmmer.plan7.Background(alphabet) #? The null(background) model can be later augmented.
+            # anonymous_msa = pyhmmer.easel.TextMSA(bytes(cached_name, 'utf-8'),sequences=seq_tuples)
+            # HMM, _profile, _optmized_profile = builder.build_msa(anonymous_msa.digitize(alphabet), background)
+
+            seqs = fasta_phylogenetic_correction(candidate_class, organism_taxid, n_neighbors=10)
+            seqs_a = muscle_align_N_seq(iter(seqs))
+
+            cached_name = "class_{}_taxid_{}.hmm".format(candidate_class, organism_taxid)
+            alphabet    = pyhmmer.easel.Alphabet.amino()
+            HMM         = hmm_create(cached_name, seqs_a, alphabet)
+
+            hmm_cache(HMM)
+            
+            #TODO: Cache it
+            return HMM
+
+
+
+        if candidate_class in RNAClassEnum:
+            ...
 
 
 # classify_sequence("CASFFASF", organism_taxid, "ribosomal_protein")
