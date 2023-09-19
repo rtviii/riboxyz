19beb083da454438cb49bd245d044c290b227939
rtviii <rtkushner@gmail.com>
2023-09-19 14:24:13 -0500

    before implementing rna classification

--- ribctl/cli/ls.py
+++ ribctl/cli/ls.py
@@ -7,10 +7,8 @@ from ribctl.etl.ribosome_assets import RibosomeAssets
 
 
 def cmd_ls(args):
-
     all_structs = os.listdir(RIBETL_DATA)
     if args.struct != None:
-
         if "." in args.struct:
             rcsb_id, auth_asym_id = args.struct.split(".")
         rp = RibosomeAssets(rcsb_id)
@@ -19,11 +17,9 @@ def cmd_ls(args):
             pprint(json.loads(chain.json()))
         else:
             pprint(json.loads(RibosomeAssets(args.struct).profile().json()))
-
-
     elif args.species != None:
         print("Listing species information for", args.species)
     elif args.subelement != None:
         print("Listing subelement information for", args.subelement)
     else:
-        print("Listing all information")
+        print(all_structs)
