91475e188345e3d9eadaf3661e803454f87b0c56
rtviii <rtkushner@gmail.com>
2023-10-02 02:00:07 -0500

    Todos

--- ribctl/lib/util_taxonomy.py
+++ ribctl/lib/util_taxonomy.py
@@ -12,27 +12,35 @@ Separately implement the source/host thing for structs.
 """
 
 
-def taxid_domain(taxid:int)->typing.Literal["bacteria","eukaryota","archaea", "virus"]:
-
-    match ( taxid_is_descendant_of(TAXID_EUKARYOTA, taxid)[0], taxid_is_descendant_of(TAXID_BACTERIA, taxid)[0], taxid_is_descendant_of(TAXID_ARCHAEA, taxid)[0] ):
-        case (False,False,True):
+def taxid_domain(
+    taxid: int,
+) -> typing.Literal["bacteria", "eukaryota", "archaea", "virus"]:
+    match (
+        taxid_is_descendant_of(TAXID_EUKARYOTA, taxid)[0],
+        taxid_is_descendant_of(TAXID_BACTERIA, taxid)[0],
+        taxid_is_descendant_of(TAXID_ARCHAEA, taxid)[0],
+    ):
+        case (False, False, True):
             return "archaea"
-        case (False,True,False):
+        case (False, True, False):
             return "bacteria"
-        case (True,False,False):
+        case (True, False, False):
             return "eukaryota"
-        case (False,False,False):
+        case (False, False, False):
             print("Probably a virus")
             return "virus"
         case _:
-            raise ValueError("Taxid {} is not a descendant of any of the three domains".format(taxid))
+            raise ValueError(
+                "Taxid {} is not a descendant of any of the three domains".format(taxid)
+            )
 
-def get_descendants_of( parent:int, targets:list[int]):
+
+def get_descendants_of(parent: int, targets: list[int]):
     ncbi = NCBITaxa()
-    
+
     # Get the taxonomic lineage of the parent tax id
     parent_lineage = ncbi.get_lineage(parent)
-    
+
     # Get the descendants of the parent tax id
     descendants = set()
 
@@ -40,30 +48,39 @@ def get_descendants_of( parent:int, targets:list[int]):
         lineage = ncbi.get_lineage(tax_id)
         if parent in lineage:
             descendants.add(tax_id)
-    
+
     print(descendants)
     return descendants
 
+
 # ? Struct-specific functions
 
-def taxid_is_descendant_of(parent_taxid: int, target_taxid:int) -> ( bool, list[int]|None ):
+
+def taxid_is_descendant_of(
+    parent_taxid: int, target_taxid: int
+) -> (bool, list[int] | None):
     ncbi = NCBITaxa()
     lineage = ncbi.get_lineage(target_taxid)
     if lineage is None:
         raise LookupError("Lineage is None. Check if taxid is NCBI-valid.")
-    return ( False,lineage ) if parent_taxid not in lineage else (True, lineage)
+    return (False, lineage) if parent_taxid not in lineage else (True, lineage)
 
-def descendants_of_taxid(struct_taxids:list[Tuple[ str,int ]],parent_taxid: int) -> list[Tuple[ str,int ]]:
-        descendants = []
-        for (rcsb_id, taxid) in struct_taxids:
-            descends,lin = taxid_is_descendant_of(parent_taxid, taxid)
-            if descends:
-                descendants.append((rcsb_id,taxid))
 
-        return descendants
+def descendants_of_taxid(
+    struct_taxids: list[Tuple[str, int]], parent_taxid: int
+) -> list[Tuple[str, int]]:
+    descendants = []
+    for rcsb_id, taxid in struct_taxids:
+        descends, lin = taxid_is_descendant_of(parent_taxid, taxid)
+        if descends:
+            descendants.append((rcsb_id, taxid))
+
+    return descendants
+
 
 def __node_lineage(node):
     return NCBITaxa().get_lineage(node.taxid)
+
 
 def __lift_rank_to_species(taxid: int) -> int:
     """Given a taxid, make sure that it's a SPECIES (as opposed to strain, subspecies, isolate, norank etc.)"""
@@ -77,5 +94,3 @@ def __lift_rank_to_species(taxid: int) -> int:
         while ncbi.get_rank([node])[node] != "species":
             node = next(lin)
         return node
-
-
