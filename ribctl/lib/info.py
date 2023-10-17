2803f81831ed1bb4a42f3d6ebf9b5659fdcaaf44
rtviii <rtkushner@gmail.com>
2023-10-17 18:52:40 -0500

    developing stats methods

--- ribctl/lib/info.py
+++ ribctl/lib/info.py
@@ -1,33 +1,78 @@
 import json
 from pprint import pprint
+import sys
 from ribctl.etl.ribosome_assets import RibosomeAssets
-from ribctl.lib.ribosome_types.types_ribosome import MitochondrialRNAClass
+from ribctl.lib.ribosome_types.types_ribosome import MitochondrialRNAClass, Polymer
+
+
+
+def struct_stats(ra:RibosomeAssets):
+    profile     = ra.profile()
+    struct_stat = {}
+
+    has_factors = False
+    has_trna    = False
+    n_dbank_compounds  = 0
+
+
+    rnas  = profile.rnas
+    prots = profile.proteins
+    ligs  = profile.nonpolymeric_ligands
+
+
+    for lig in ligs:
+        if lig.nonpolymer_comp.drugbank != None:
+            n_dbank_compounds += 1
+            print(lig.nonpolymer_comp.drugbank)
+        else:
+            print("No drugbank")
+    
+    
+    def lsu_ssu_presence(rnas:list[Polymer]):
+        has_lsu     = 0
+        has_ssu     = 0
+        for rna in rnas:
+            if profile.mitochondrial :
+                if  "mt12SrRNA" in rna.nomenclature:
+                    has_ssu = 1
+                elif  "mt16SrRNA" in rna.nomenclature:
+                    has_lsu = 2
+
+        match has_ssu + has_lsu:
+            case 1:
+                return "ssu"
+            case 2:
+                return "lsu"
+            case 3:
+                return "both"
+
+        
+            
+            
+
+
+    
+
 
 
 def get_stats():
-    struct_stat = {
-        "taxid":[]
-    }
 
+    global_stats = {
+        "mitochondrial"     : 0,
+        "cytosolic"         : 0,
+        "lsu_only"          : 0,
+        "ssu_only"          : 0,
+        "ssu_lsu"           : 0,
+        "with_trna"         : 0,
+        "with_factor"       : 0,
+        "drugbank_compounds": 0
+    }
+    chain_classes={}
+       
     for struct in RibosomeAssets.list_all_structs():
 
-    # struct = "2FTC"
-        print(struct)
-        ra                 = RibosomeAssets(struct)
-        profile            = ra.profile()
-        d                  = profile.json()
-        d                  = json.loads(d)
-        d['mitochondrial'] = False
-        for rna in profile.rnas:
-            if len( rna.nomenclature )>0 :
-               if ( rna.nomenclature[0].value in [k.value for k in list(MitochondrialRNAClass)] ):
-                    print("{} : Mitochodrial rna {}".format(struct, rna.nomenclature), )
-                    d['mitochondrial'] = True
-                    break
-        ra.write_own_json_profile(d, overwrite=True)
-        taxid = profile.src_organism_ids[0]
 
 
 
 
-get_stats()
+struct_stats(RibosomeAssets(sys.argv[1].upper()))
