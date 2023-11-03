cc9fdf85f12bda9f295dd10d5a4c633f3a073e87
rtviii <rtkushner@gmail.com>
2023-11-02 20:18:08 -0500

    enum union name/value is a major bitch

--- ribctl/lib/ribosome_types/types_binding_site.py
+++ ribctl/lib/ribosome_types/types_binding_site.py
@@ -79,10 +79,10 @@ class BindingSite(BaseModel):
         RIBETL_DATA = os.environ.get('RIBETL_DATA')
         return os.path.join( str(RIBETL_DATA), rcsb_id.upper() , "LIGAND_" + class_.replace(" ", "_").upper() + ".json")
 
-    @staticmethod
-    def path_poly_factor( rcsb_id: str, class_: str, auth_asym_id: str):
-        RIBETL_DATA = os.environ.get('RIBETL_DATA')
-        return os.path.join( str(RIBETL_DATA), rcsb_id.upper(), "POLYMER_"+auth_asym_id +  "_" + class_.replace(" ", "_").upper() + ".json" )
+    # @staticmethod
+    # def path_poly_factor( rcsb_id: str, class_: str, auth_asym_id: str):
+    #     RIBETL_DATA = os.environ.get('RIBETL_DATA')
+    #     return os.path.join( str(RIBETL_DATA), rcsb_id.upper(), "POLYMER_"+auth_asym_id +  "_" + class_.replace(" ", "_").upper() + ".json" )
 
     def save(self, filename: str):
         with open(filename, 'w') as outfile:
