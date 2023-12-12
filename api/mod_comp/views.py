c618dade1a847d1e73c282d42c95571dea2a2390
rtviii <rtkushner@gmail.com>
2023-12-11 16:41:51 -0800

    endpoint for chimerax bundle testing

--- api/mod_comp/views.py
+++ api/mod_comp/views.py
@@ -1,8 +1,12 @@
+import json
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
+from wsgiref.util import FileWrapper
 import os
 from rbxz_bend.settings import RIBETL_DATA
 from pymol import cmd
@@ -18,9 +22,7 @@ def edit_chain(request):
 
     params     = dict(request.GET)
     structid   = str.upper(params['structid'][0])
-
     filehandle = os.path.join(RIBETL_DATA, structid + ".cif")
-
     cmd.select("resi 1-3")
     cmd.create("{}_test".format(structid), "sele")
     tosave=os.path.join(RIBETL_DATA, f"tempchain{structid}.cif")
@@ -28,4 +30,30 @@ def edit_chain(request):
     cmd.save(tosave)
 
     return Response("Edited chain successfully")
+
+
+@api_view(['GET'])
+def get_chain(request):
+    params   = dict(request.GET)
+    chainid  = params['auth_asym_id'][0]
+    structid = str.upper(params['rcsb_id'][0])
+    filename = "{}_STRAND_{}.cif".format(structid, chainid)
+
+    file_handle = os.path.join(RIBETL_DATA,structid,"CHAINS", filename)
+
+    document = open(file_handle, 'rb')
+    response = HttpResponse(FileWrapper(document), content_type='chemical/x-mmcif')
+    response['Content-Disposition'] = 'attachment; filename="{}_subchain_{}.cif"'.format(structid, chainid)
+    return response
+
+@api_view(['GET'])
+def get_profile(request):
+    params   = dict(request.GET)
+    rcsb_id = str.upper(params['rcsb_id'][0])
+    filename = "{}.json".format(rcsb_id)
+    file_handle = os.path.join(RIBETL_DATA,rcsb_id, filename)
+
+    with open(file_handle, 'rb') as f:
+        profile = json.load(f)
+        return Response(profile)
 
