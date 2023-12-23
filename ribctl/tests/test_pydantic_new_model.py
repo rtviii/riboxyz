bb547dffa67c44b61224f336245b65349ef4d693
rtviii <rtkushner@gmail.com>
2023-12-22 16:31:51 -0800

    test this

--- ribctl/tests/test_pydantic_new_model.py
+++ ribctl/tests/test_pydantic_new_model.py
@@ -89,7 +89,6 @@ class Polymer(BaseModel):
         return [x.name for x in nomenclature]
     nomenclature:  list[PolymerClass]
 
-
 class Protein(Polymer):
 
 
@@ -129,10 +128,151 @@ class Protein(Polymer):
         return Polymer(**self.model_dump())
 
 
+class RNA(Polymer):
+    def __hash__(self):
+        return hash(self.auth_asym_id + self.parent_rcsb_id)
 
-with open('test.json', 'r') as f:
-    poly = Polymer.model_validate(json.load(f))
-    print("Uploaded poly", poly)
 
-with open('test1.json', 'w') as f:
-    json.dump(poly.model_dump(), f)
+class NonpolymericLigand(BaseModel)  : 
+
+      class NonpolymerComp(BaseModel):
+        class ChemComp(BaseModel):
+            class ChemCompContainerIdentifiers(BaseModel):
+                id: str
+                name: str
+                three_letter_code: str
+            chem_comp_container_identifiers: ChemCompContainerIdentifiers
+
+        class Drugbank(BaseModel):
+            class DrugbankInfo(BaseModel):
+                cas_number: Optional[str]
+                description: Optional[str]
+            class DrugbankContainerIdentifiers(BaseModel):
+                drugbank_id: str
+            drugbank_container_identifiers: DrugbankContainerIdentifiers
+            drugbank_info: DrugbankInfo
+
+        class RcsbChemCompTarget(BaseModel):
+
+            interaction_type                 : Optional[str] 
+            name                             : Optional[str] 
+            provenance_source                : Optional[str] 
+            reference_database_accession_code: Optional[str] 
+            reference_database_name          : Optional[str] 
+
+        
+        chemp_comp           : Optional[ChemComp]
+        drugbank             : Optional[Drugbank]
+        rcsb_chem_comp_target: Optional[list[RcsbChemCompTarget]]
+
+      chemicalId         : str
+      chemicalName       : str
+      formula_weight     : Optional[float]
+
+      pdbx_description   : str
+      number_of_instances: int
+
+      nonpolymer_comp: NonpolymerComp
+        
+
+class AssemblyInstancesMap(BaseModel):
+    """
+    This basically specifies which assembly an instnace of a polymer or a nonpolymer belongs to. 
+    Certain PDB structures come with more than a single physical model/assembly packaged in the file,
+    hence every chain and many ligands might be present in 2 or more instances. 
+    
+    The RNA/Protein/Ligand suffices to characterizes all instances, yet, to resolve duplicate chains 
+    precisely in space, this information is needed.
+    
+    assemblies{
+    rcsb_id 
+   	nonpolymer_entity_instances{
+          rcsb_nonpolymer_entity_instance_container_identifiers{
+            entity_id
+            comp_id
+            auth_asym_id
+            rcsb_id
+            auth_seq_id
+          }
+        }
+        polymer_entity_instances{
+           rcsb_polymer_entity_instance_container_identifiers {  
+            entity_id
+            asym_id
+            auth_asym_id
+            entry_id
+            entity_id
+          }
+        }
+      }
+    """
+
+    class NonpolymerEntityInstance(BaseModel):
+
+        class NonpolymerEntityInstanceContainerIdentifiers(BaseModel):
+
+            entity_id   : str
+            auth_asym_id: str
+            auth_seq_id : str
+
+        rcsb_nonpolymer_entity_instance_container_identifiers: NonpolymerEntityInstanceContainerIdentifiers
+
+    class PolymerEntityInstance(BaseModel):
+        class PolymerEntityInstanceContainerIdentifiers(BaseModel):
+            entity_id: str
+            auth_asym_id: str
+
+        rcsb_polymer_entity_instance_container_identifiers: PolymerEntityInstanceContainerIdentifiers
+
+    rcsb_id                    : str # ex. 5AFI-1
+    nonpolymer_entity_instances: Optional[list[NonpolymerEntityInstance]]
+    polymer_entity_instances   : list[PolymerEntityInstance]
+
+
+class RibosomeStructure(BaseModel):
+
+    rcsb_id   : str
+    expMethod : str
+    resolution: float
+
+
+    pdbx_keywords         : Optional[str]
+    pdbx_keywords_text    : Optional[str]
+
+    rcsb_external_ref_id  : list[str]
+    rcsb_external_ref_type: list[str]
+    rcsb_external_ref_link: list[str]
+
+    citation_year        : Optional[int]
+    citation_rcsb_authors: Optional[list[str]]
+    citation_title       : Optional[str]
+    citation_pdbx_doi    : Optional[str]
+
+    src_organism_ids  : list[int]
+    src_organism_names: list[str]
+
+    host_organism_ids  : list[int]
+    host_organism_names: list[str]
+
+    assembly_map: list[AssemblyInstancesMap]
+    mitochondrial: bool
+
+    # proteins            : list[Any]
+    # rnas                : list[Any]
+    # nonpolymeric_ligands: list[Any]
+    # polymeric_factors   : list[Any]
+
+    proteins            : list[Protein]
+    rnas                : list[RNA]
+
+    # polymeric_factors   : list[LifecycleFactor]
+    #? This includes DNA-RNA hybrid strands, DNA and all other polymers
+    other_polymers   : list[Polymer]
+
+    nonpolymeric_ligands: list[NonpolymericLigand]
+
+    
+    # TODO: Deprecate this
+    # @staticmethod
+    # def from_json_profile(d: Any):
+    #     return RibosomeStructure(**d)
