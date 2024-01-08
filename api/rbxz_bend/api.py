e6d5786003cd3e736e59f4acf1814679c45a9af9
rtviii <rtkushner@gmail.com>
2024-01-07 16:11:23 -0800

    structure router

--- api/rbxz_bend/api.py
+++ api/rbxz_bend/api.py
@@ -1,7 +1,7 @@
 from ninja import NinjaAPI
+from .api_v0_router import apiv0
+from .api_structure_router import structure_router
 
 root_api = NinjaAPI(docs_url='/docs', openapi_url='/openapi.json', title='riboxyz-api', description='A programming interface for ribosome data.')
-
-@root_api.get("/hello")
-def hello(request):
-    return "Hello world"
+root_api.add_router('/', apiv0)
+root_api.add_router('/structure', structure_router)
