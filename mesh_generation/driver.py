ab96504bc91f262e934e26e2f08981eb861c00c7
rtviii <rtkushner@gmail.com>
2024-01-28 22:40:42 -0800

    atom aggregation

--- mesh_generation/driver.py
+++ mesh_generation/driver.py
@@ -1,8 +1,10 @@
 
-from ribctl.lib.tunnel import open_tunnel_csv, parse_struct_via_centerline
+from ribctl.lib.tunnel import encode_atoms, open_tunnel_csv, parse_struct_via_centerline
 
 
 RCSB_ID = '6Z6K'
 
 data = open_tunnel_csv(RCSB_ID)
-parse_struct_via_centerline(RCSB_ID,data)
+atoms = parse_struct_via_centerline(RCSB_ID,data)
+
+print(encode_atoms(RCSB_ID,atoms))
