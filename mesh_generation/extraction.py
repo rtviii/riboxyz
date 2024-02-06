14361070498d8ff17cf9f404d27860477c43b442
rtviii <rtkushner@gmail.com>
2024-02-05 16:14:33 -0800

    coloration

--- mesh_generation/extraction.py
+++ mesh_generation/extraction.py
@@ -49,7 +49,7 @@ def open_tunnel_csv(rcsb_id: str) -> list[list]:
 
 def parse_struct_via_centerline(
     rcsb_id: str, centerline_data: list, expansion_radius: int = 15
-) -> list:
+) -> list[Atom]:
     """centerline data is an array of lists [radius, x, y, z]"""
     from Bio.PDB.MMCIFParser import MMCIFParser
     from Bio.PDB.NeighborSearch import NeighborSearch
@@ -159,58 +159,9 @@ def create_pcd_from_atoms(
     o3d.io.write_point_cloud(save_path, pcd)
 
 
-# TODO : 1. Remove ions and non-standard residues when refining the tunnel
-# TODO : CHECK. make the centerline scan in refinement dynamic (on radius of probe)
-
-
-def get_sphere_indices_voxelized(center: np.ndarray, radius: int):
-    """Make sure radius reflects the size of the underlying voxel grid"""
-    x0, y0, z0 = center
-
-    #!------ Generate indices of a voxel cube of side 2r+2  around the centerpoint
-    x_range = slice(int(np.floor(x0) - (radius)), int(np.ceil(x0) + (radius)))
-    y_range = slice(int(np.floor(y0) - (radius)), int(np.ceil(y0) + (radius)))
-    z_range = slice(int(np.floor(z0) - (radius)), int(np.ceil(z0) + (radius)))
-
-    indices = np.indices(
-        (
-            x_range.stop - x_range.start,
-            y_range.stop - y_range.start,
-            z_range.stop - z_range.start,
-        )
-    )
-
-    indices += np.array([x_range.start, y_range.start, z_range.start])[
-        :, np.newaxis, np.newaxis, np.newaxis
-    ]
-    indices = indices.transpose(1, 2, 3, 0)
-    indices_list = list(map(tuple, indices.reshape(-1, 3)))
-    #!------ Generate indices of a voxel cube of side 2r+2  around the centerpoint
-
-    sphere_active_ix = []
-
-    for ind in indices_list:
-        x_ = ind[0]
-        y_ = ind[1]
-        z_ = ind[2]
-        if (x_ - x0) ** 2 + (y_ - y0) ** 2 + (z_ - z0) ** 2 <= radius**2:
-            sphere_active_ix.append([x_, y_, z_])
-
-    return np.array(sphere_active_ix)
-
-
-def cloud_get_bbox(coords: np.ndarray):
-
-    bbox = bounding_box()
-    return bbox
-
-
-
 
 RCSB_ID = sys.argv[1].upper()
 cloud   = parse_struct_via_centerline(RCSB_ID, open_tunnel_csv(RCSB_ID))
-bbox    = bounding_box(cloud)
-print("Got cloud," , len(cloud), "atoms")
-print("got bbox", bbox)
-# atoms   = parse_struct_via_bbox(RCSB_ID, bbox)
-# encode_atoms(RCSB_ID, atoms, write=True)
+bbox    = bounding_box(np.array([a.get_coord() for a in cloud]))
+atoms   = parse_struct_via_bbox(RCSB_ID, bbox)
+encode_atoms(RCSB_ID, atoms, write=True)
