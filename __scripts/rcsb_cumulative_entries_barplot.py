2f025745e6cc8079cef51032857674bce1fa9764
rtviii <rtkushner@gmail.com>
2024-05-08 13:38:05 -0700

    appeneded readme, some much needed cleanup

---
+++ __scripts/rcsb_cumulative_entries_barplot.py
@@ -0,0 +1,194 @@
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+import datetime
+from matplotlib import rc, rcParams
+
+
+dates_query = """{
+  "query": {
+    "type": "group",
+    "nodes": [
+      {
+        "type": "terminal",
+        "label": "text",
+        "service": "text",
+        "parameters": {
+          "operator": "contains_phrase",
+          "negation": false,
+          "value": "RIBOSOME",
+          "attribute": "struct_keywords.pdbx_keywords"
+        }
+      },
+      {
+        "type": "terminal",
+        "service": "text_chem",
+        "parameters": {
+          "operator": "in",
+          "negation": false,
+          "value": [
+            "ERY"
+          ],
+          "attribute": "rcsb_chem_comp_container_identifiers.comp_id"
+        }
+      }
+    ],
+    "logical_operator": "and"
+  },
+  "return_type": "entry",
+  "request_options": {
+    "scoring_strategy": "combined",
+    "sort": [
+      {
+        "sort_by": "score",
+        "direction": "desc"
+      }
+    ],
+    "paginate": {
+      "start": 0,
+      "rows": 25
+    },
+    "results_content_type": [
+      "experimental"
+    ]
+  }
+}"""
+# open the structs.json file and read the data 
+
+
+
+with open('custom_report.json', 'r') as infile:
+    data = json.load(infile)
+
+dates   = []
+heights = []
+_       = 0
+
+by_year  = {
+    '2000':{
+        'xray'        : 0,
+        'em'          : 0,
+        'emdb_maps'   : 0,
+        'xray_resos'  : [],
+        'cryoem_resos': []
+    }
+}
+
+XRAY = 'X-RAY DIFFRACTION'
+NMR  = 'SOLUTION NMR'
+EM   = 'ELECTRON MICROSCOPY'
+
+
+for s in data:
+    datestr          = str(s['data']['rcsb_accession_info']['initial_release_date']).split("T")[0]
+    method           = str(s['data']['exptl'][0]['method']);
+    reso             = s['data']['rcsb_entry_info']['resolution_combined']
+    maps             = s['data']['rcsb_entry_container_identifiers']['emdb_ids']
+    [year,month,day] = datestr.split("-")
+
+    if reso == None :
+        continue
+
+
+    if int(year) < 2000:
+        by_year['2000']['emdb_maps'] +=  len (maps) if maps != None else 0
+
+        if method == XRAY:
+            by_year['2000']['xray'] += 1
+            by_year['2000']['xray_resos'] += reso
+        elif method == NMR:
+            ...
+        elif method == EM:
+            by_year['2000']['em'] += 1
+            by_year['2000']['cryoem_resos'] += reso
+        
+    else:
+
+      if year not in by_year:
+          by_year[year] = {
+          'xray'        : 0,
+          'em'          : 0,
+          'emdb_maps'   : 0,
+          'xray_resos'  : [],
+          'cryoem_resos': []
+          }
+
+      by_year[year]['emdb_maps'] +=  len (maps) if maps != None else 0
+      if method == XRAY:
+              by_year[year]['xray'] += 1
+              by_year[year]['xray_resos'] += reso
+
+      elif method == NMR:
+          ...
+      elif method == EM:
+              by_year[year]['em'] += 1
+              by_year[year]['cryoem_resos'] += reso
+    d = datetime.datetime(int(year),int(month),int(day))
+
+    dates.append(d)
+    heights.append(_)
+    
+by_year = sorted(by_year.items())
+# ------------------------------------------------------------
+
+d_years       = []
+d_ems         = []
+d_xrays       = []
+
+d_num_maps    = []
+d_cryoem_reso = []
+d_xray_reso   = []
+
+for e in by_year:
+    _em   = e[1]['em']
+    _xray = e[1]['xray']
+    _xray_reso = np.mean(e[1]['xray_resos'])
+    _cryoem_reso = np.mean(e[1]['cryoem_resos'])
+    _num_maps = e[1]['emdb_maps']
+    d_years.append(int(e[0]))
+    d_ems.append(_em)
+    d_xrays.append(_xray)
+    d_num_maps.append(_num_maps)
+    d_xray_reso.append(_xray_reso)
+    d_cryoem_reso.append(_cryoem_reso)
+
+width         = 0.65       # the width of the bars: can also be len(x) sequence
+fig, ax = plt.subplots()
+rc('axes', linewidth=2)
+xraybars = ax.bar(d_years, d_xrays, width,                label='X-RAY DIFFRACTION' , color="black"   , edgecolor="black")
+embars   = ax.bar(d_years, d_ems  , width, bottom=d_xrays,label='ELECTRON MICROSCOPY'  ,fill=None , edgecolor="black")
+# ax.scatter(d_years, d_num_maps, color="blue", marker='*', label="Associated EMDB maps", s=150)
+# ax.plot(d_years, d_cryoem_reso, color="cyan", label="Cryo-EM resolution", linewidth=3)
+# ax.plot(d_years, d_xray_reso, color="pink", label="X-ray resolution", linewidth=3)
+# ax.lines[0].set_linestyle("--")
+plt.xticks(fontsize=22)
+plt.yticks(fontsize=22)
+# ax.set_ylabel('Scores')
+# ax.set_title('Scores by group and gender')
+ax.legend()
+
+print("nume entires", len(d_years))
+#  set x axis label to "Year"
+
+ax.set_xlabel('Year'                      , fontsize=24)
+# ax.set_ylabel('Number of released RCSB models' , fontsize=24)
+# title= "Number of entries in $\it{ribosome.xyz}$ by year"
+title= "Number of ribosome structures deposited to $\it{rcsb.org}$"
+ax.set_title (title, fontsize=24)
+plt.legend(loc=2, prop={'size': 22})
+
+
+# ax.set_xticklabels(['2000', '2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019'])
+
+
+for ( bar, i_em, i_xray ) in zip(embars.patches, d_ems, d_xrays):
+    value = bar.get_height()
+    text = f'{i_em+i_xray}'
+    text_x = bar.get_x() + bar.get_width() / 2
+    text_y = bar.get_y() + value
+    ax.text(text_x, text_y + 3, text, ha='center',color='black',size=18)
+
+fig.set_size_inches(18.5, 10.5)
+plt.show()
+fig.savefig('cumulative_entries.png', format='png', dpi=300, bbox_inches='tight')
+fig.savefig('cumulative_entries.pdf', format='pdf', dpi=300, bbox_inches='tight')
---
+++ __scripts/williamson_assembly.py
@@ -0,0 +1,165 @@
+
+from concurrent.futures import ThreadPoolExecutor
+import os
+import urllib.request
+
+def download_file(url, dest):
+    try:
+        urllib.request.urlretrieve(url, dest)
+        print(f"File downloaded and saved to {dest}")
+    except Exception as e:
+        print(f"An error occurred while downloading {url}: {str(e)}")
+
+assembly_stages = {
+"dead-B-a1" : "https://www.ebi.ac.uk/emdb/EMD-40517",
+"dead-B-a2" : "https://www.ebi.ac.uk/emdb/EMD-40519",
+"dead-B-a3" : "https://www.ebi.ac.uk/emdb/EMD-40520",
+"dead-B-a4" : "https://www.ebi.ac.uk/emdb/EMD-40524",
+"dead-B-a5" : "https://www.ebi.ac.uk/emdb/EMD-40526",
+"dead-B-a6" : "https://www.ebi.ac.uk/emdb/EMD-40528",
+"dead-B-b1" : "https://www.ebi.ac.uk/emdb/EMD-40530",
+"dead-C-a1" : "https://www.ebi.ac.uk/emdb/EMD-40532",
+"dead-C-a2" : "https://www.ebi.ac.uk/emdb/EMD-40534",
+"dead-C-a3" : "https://www.ebi.ac.uk/emdb/EMD-40536",
+"dead-C-a4" : "https://www.ebi.ac.uk/emdb/EMD-40538",
+"dead-C-a5" : "https://www.ebi.ac.uk/emdb/EMD-40540",
+"dead-C-b1" : "https://www.ebi.ac.uk/emdb/EMD-40542",
+"dead-C-b2" : "https://www.ebi.ac.uk/emdb/EMD-40544",
+"dead-E-a1" : "https://www.ebi.ac.uk/emdb/EMD-40546",
+"dead-G1"   : "https://www.ebi.ac.uk/emdb/EMD-40548",
+"dead-G2"   : "https://www.ebi.ac.uk/emdb/EMD-40550",
+"dead-G3"   : "https://www.ebi.ac.uk/emdb/EMD-40552",
+"dead-G4"   : "https://www.ebi.ac.uk/emdb/EMD-40555",
+"dead-preB1": "https://www.ebi.ac.uk/emdb/EMD-40551",
+"dead-preB2": "https://www.ebi.ac.uk/emdb/EMD-40549",
+"rl17-B-b1" : "https://www.ebi.ac.uk/emdb/EMD-40309",
+"rl17-B-b2" : "https://www.ebi.ac.uk/emdb/EMD-40311",
+"rl17-B-b3" : "https://www.ebi.ac.uk/emdb/EMD-40313",
+"rl17-B-b4" : "https://www.ebi.ac.uk/emdb/EMD-40511",
+"rl17-C-b1" : "https://www.ebi.ac.uk/emdb/EMD-40314",
+"rl17-C-b2" : "https://www.ebi.ac.uk/emdb/EMD-40315",
+"rl17-C-b3" : "https://www.ebi.ac.uk/emdb/EMD-40317",
+"rl17-C-b4" : "https://www.ebi.ac.uk/emdb/EMD-40319",
+"rl17-C-b5" : "https://www.ebi.ac.uk/emdb/EMD-40321",
+"rl17-C-b6" : "https://www.ebi.ac.uk/emdb/EMD-40323",
+"rl17-D-a1" : "https://www.ebi.ac.uk/emdb/EMD-40327",
+"rl17-D-a2" : "https://www.ebi.ac.uk/emdb/EMD-40329",
+"rl17-D-a3" : "https://www.ebi.ac.uk/emdb/EMD-40331",
+"rl17-D-b1" : "https://www.ebi.ac.uk/emdb/EMD-40333",
+"rl17-D-b2" : "https://www.ebi.ac.uk/emdb/EMD-40512",
+"rl17-D-b3" : "https://www.ebi.ac.uk/emdb/EMD-40514",
+"rl17-D-b4" : "https://www.ebi.ac.uk/emdb/EMD-40516",
+"rl17-E-a1" : "https://www.ebi.ac.uk/emdb/EMD-40518",
+"rl17-E-a2" : "https://www.ebi.ac.uk/emdb/EMD-40521",
+"rl17-E-a3" : "https://www.ebi.ac.uk/emdb/EMD-40523",
+"rl17-E-a4" : "https://www.ebi.ac.uk/emdb/EMD-40525",
+"rl17-E-a5" : "https://www.ebi.ac.uk/emdb/EMD-40527",
+"rl17-E-b1" : "https://www.ebi.ac.uk/emdb/EMD-40529",
+"rl17-E-b10": "https://www.ebi.ac.uk/emdb/EMD-40531",
+"rl17-E-b2" : "https://www.ebi.ac.uk/emdb/EMD-40533",
+"rl17-E-b3" : "https://www.ebi.ac.uk/emdb/EMD-40535",
+"rl17-E-b4" : "https://www.ebi.ac.uk/emdb/EMD-40537",
+"rl17-E-b5" : "https://www.ebi.ac.uk/emdb/EMD-40539",
+"rl17-E-b6" : "https://www.ebi.ac.uk/emdb/EMD-40541",
+"rl17-E-b7" : "https://www.ebi.ac.uk/emdb/EMD-40543",
+"rl17-E-b8" : "https://www.ebi.ac.uk/emdb/EMD-40545",
+"rl17-E-b9" : "https://www.ebi.ac.uk/emdb/EMD-40547",
+"srmb-B-a1" : "https://www.ebi.ac.uk/emdb/EMD-40316",
+"srmb-B-a2" : "https://www.ebi.ac.uk/emdb/EMD-40318",
+"srmb-C-a1" : "https://www.ebi.ac.uk/emdb/EMD-40320",
+"srmb-C-a2" : "https://www.ebi.ac.uk/emdb/EMD-40322",
+"srmb-C-a3" : "https://www.ebi.ac.uk/emdb/EMD-40324",
+"srmb-C-a4" : "https://www.ebi.ac.uk/emdb/EMD-40325",
+"srmb-C-a5" : "https://www.ebi.ac.uk/emdb/EMD-40328",
+"srmb-E-a1" : "https://www.ebi.ac.uk/emdb/EMD-40330",
+"srmb-E-a2" : "https://www.ebi.ac.uk/emdb/EMD-40332",
+"srmb-E-a3" : "https://www.ebi.ac.uk/emdb/EMD-40513",
+"srmb-G1"   : "https://www.ebi.ac.uk/emdb/EMD-4051"}
+
+assembly_intermediates = [
+"https://www.ebi.ac.uk/emdb/EMD-40517",
+"https://www.ebi.ac.uk/emdb/EMD-40519",
+"https://www.ebi.ac.uk/emdb/EMD-40520",
+"https://www.ebi.ac.uk/emdb/EMD-40524",
+"https://www.ebi.ac.uk/emdb/EMD-40526",
+"https://www.ebi.ac.uk/emdb/EMD-40528",
+"https://www.ebi.ac.uk/emdb/EMD-40530",
+"https://www.ebi.ac.uk/emdb/EMD-40532",
+"https://www.ebi.ac.uk/emdb/EMD-40534",
+"https://www.ebi.ac.uk/emdb/EMD-40536",
+"https://www.ebi.ac.uk/emdb/EMD-40538",
+"https://www.ebi.ac.uk/emdb/EMD-40540",
+"https://www.ebi.ac.uk/emdb/EMD-40542",
+"https://www.ebi.ac.uk/emdb/EMD-40544",
+"https://www.ebi.ac.uk/emdb/EMD-40546",
+"https://www.ebi.ac.uk/emdb/EMD-40548",
+"https://www.ebi.ac.uk/emdb/EMD-40550",
+"https://www.ebi.ac.uk/emdb/EMD-40552",
+"https://www.ebi.ac.uk/emdb/EMD-40555",
+"https://www.ebi.ac.uk/emdb/EMD-40551",
+"https://www.ebi.ac.uk/emdb/EMD-40549",
+"https://www.ebi.ac.uk/emdb/EMD-40309",
+"https://www.ebi.ac.uk/emdb/EMD-40311",
+"https://www.ebi.ac.uk/emdb/EMD-40313",
+"https://www.ebi.ac.uk/emdb/EMD-40511",
+"https://www.ebi.ac.uk/emdb/EMD-40314",
+"https://www.ebi.ac.uk/emdb/EMD-40315",
+"https://www.ebi.ac.uk/emdb/EMD-40317",
+"https://www.ebi.ac.uk/emdb/EMD-40319",
+"https://www.ebi.ac.uk/emdb/EMD-40321",
+"https://www.ebi.ac.uk/emdb/EMD-40323",
+"https://www.ebi.ac.uk/emdb/EMD-40327",
+"https://www.ebi.ac.uk/emdb/EMD-40329",
+"https://www.ebi.ac.uk/emdb/EMD-40331",
+"https://www.ebi.ac.uk/emdb/EMD-40333",
+"https://www.ebi.ac.uk/emdb/EMD-40512",
+"https://www.ebi.ac.uk/emdb/EMD-40514",
+"https://www.ebi.ac.uk/emdb/EMD-40516",
+"https://www.ebi.ac.uk/emdb/EMD-40518",
+"https://www.ebi.ac.uk/emdb/EMD-40521",
+"https://www.ebi.ac.uk/emdb/EMD-40523",
+"https://www.ebi.ac.uk/emdb/EMD-40525",
+"https://www.ebi.ac.uk/emdb/EMD-40527",
+"https://www.ebi.ac.uk/emdb/EMD-40529",
+"https://www.ebi.ac.uk/emdb/EMD-40531",
+"https://www.ebi.ac.uk/emdb/EMD-40533",
+"https://www.ebi.ac.uk/emdb/EMD-40535",
+"https://www.ebi.ac.uk/emdb/EMD-40537",
+"https://www.ebi.ac.uk/emdb/EMD-40539",
+"https://www.ebi.ac.uk/emdb/EMD-40541",
+"https://www.ebi.ac.uk/emdb/EMD-40543",
+"https://www.ebi.ac.uk/emdb/EMD-40545",
+"https://www.ebi.ac.uk/emdb/EMD-40547",
+"https://www.ebi.ac.uk/emdb/EMD-40316",
+"https://www.ebi.ac.uk/emdb/EMD-40318",
+"https://www.ebi.ac.uk/emdb/EMD-40320",
+"https://www.ebi.ac.uk/emdb/EMD-40322",
+"https://www.ebi.ac.uk/emdb/EMD-40324",
+"https://www.ebi.ac.uk/emdb/EMD-40325",
+"https://www.ebi.ac.uk/emdb/EMD-40328",
+"https://www.ebi.ac.uk/emdb/EMD-40330",
+"https://www.ebi.ac.uk/emdb/EMD-40332",
+"https://www.ebi.ac.uk/emdb/EMD-40513",
+"https://www.ebi.ac.uk/emdb/EMD-40515"]
+
+def extract_emdb_id(emdb_url:str):
+    # "https://www.ebi.ac.uk/emdb/EMD-40517",
+    emdb_id  = emdb_url.split('/')[-1]
+    filename = emdb_id.replace("-","_").lower() + ".map.gz"
+    return emdb_id, filename
+
+
+
+with ThreadPoolExecutor(max_workers=50) as executor:
+    # Submit download tasks for each URL
+    for url in assembly_intermediates:
+        emdb_id, filename = extract_emdb_id(url)
+        try:
+            url         = "https://ftp.ebi.ac.uk/pub/databases/emdb/structures/{}/map/{}".format(emdb_id, filename)
+            destination = os.path.join('/home/rtviii/dev/riboxyz/scripts/assembly_intermediates', filename)
+            if not os.path.exists(destination):
+                executor.submit(download_file, url, destination)
+            else:
+                print("Skipping. EXISTS: {} ".format(destination))
+        except:
+            print("NO EMD LINK FOR {}".format(emdb_id))
