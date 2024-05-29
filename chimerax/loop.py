87c940da90da7ed6ef8bcdc2513324712a241512
rtviii <rtkushner@gmail.com>
2024-05-29 00:14:01 -0700

    fixed script to ignore processed structs

--- chimerax/loop.py
+++ chimerax/loop.py
@@ -1,5 +1,16 @@
 import os
 from chimerax.core.commands import run
+from concurrent.futures import ThreadPoolExecutor
 
-for i in ["7K00", "4UG0"]:
-    run(session,"ribmovie {}".format(i))
+DEST_DIR = "/home/rtviii/dev/riboxyz/chimerax/movies"
+
+for rcsb_id in ["7K00", "4UG0"]:
+    movie_dir = os.path.join(DEST_DIR, "{}.mp4".format(rcsb_id))
+    print("Attempting to write ", movie_dir)
+    print("Listdir:", os.listdir(DEST_DIR))
+
+    if os.path.join(DEST_DIR, rcsb_id, ".mp4") in os.listdir(DEST_DIR):
+        print("Skipping {}. Exists".format(rcsb_id))
+        continue
+    else:
+        run(session,"ribmovie {}".format(rcsb_id))
