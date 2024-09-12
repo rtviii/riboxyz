16f323637fd53a94b2c4ddc4d9fc51ed030177ec
rtviii <rtkushner@gmail.com>
2024-09-12 16:18:52 -0500

    gameplan

---
+++ ribctl/etl/collector.py
@@ -0,0 +1,7 @@
+# the basic recipe should be:
+
+# 1. fetch whatever data you need from rcsb
+# 2. perform transfomrations on it
+# 3. return the intermediate types and combine 
+
+# Let's have a collector class that will orchestrate this and do the housekeeping (assembly ids and shit like that).
