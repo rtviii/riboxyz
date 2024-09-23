5eb7b7f1e94b4446f87dcfd898b21f2ab701c6f6
rtviii <rtkushner@gmail.com>
2024-09-22 21:58:39 -0500

    threadpool impl

--- test.py
+++ test.py
@@ -1,15 +1,18 @@
 from pprint import pprint
 import sys
 from pydantic import BaseModel
+from Bio.PDB.Residue import Residue
 from ribctl.lib.libbsite import map_motifs
 from ribctl.lib.libseq import BiopythonChain
 from ribctl.lib.schema.types_binding_site import ResidueSummary
 from ribctl.lib.schema.types_ribosome import PolymerClass
+from functools import partial
 sys.path.append('/home/rtviii/dev/riboxyz')
 from ribctl.lib.libmsa import Fasta
 from Bio.SeqRecord import SeqRecord
 from pydantic import BaseModel
 from typing import Dict, List
+from concurrent.futures import ProcessPoolExecutor
 
 
 # Ok let's not complicate things. The prototype is thus:
@@ -30,12 +33,14 @@ class SequenceProjection_ManyToOne():
     """Given multiple sequences and residue ranges within them, project the ranges onto a single sequence
     """
 
-    target_chain : BiopythonChain
-    target_struct: RCSB_ID
-    polymer_class: PolymerClass
-    source_chains: dict[RCSB_ID,tuple[BiopythonChain,list[ResidueSummary]]]
-    mappings     : dict[RCSB_ID,dict[ResidiueIndices, ResidiueIndices]]
-    weights      : dict[int, list[RCSB_ID]]
+    target_chain   : BiopythonChain
+    target_struct  : RCSB_ID
+    polymer_class  : PolymerClass
+
+    source_chains  : dict[RCSB_ID,tuple[BiopythonChain,list[ResidueSummary]]]
+    target_mappings: dict[RCSB_ID,list[Residue]]
+
+    weights        : dict[int, list[RCSB_ID]]
 
     def __init__(self,target_seq:BiopythonChain, target_rcsb_id: str,source_seq_pairs:list[tuple[RCSB_ID,BiopythonChain,list[ResidueSummary]]], polymer_class:str) -> None:
 
@@ -47,28 +52,29 @@ class SequenceProjection_ManyToOne():
             self.source_chains[rcsb_id] = (chain,indices)
 
     def project(self):
+        def map_motifs_wrapper(args):
+            source_chain, target_chain, source_residues, polymer_class = args
+            return map_motifs(source_chain, target_chain, source_residues, polymer_class)
 
-        # TODO: put this on a threadpool 
-        # with concurrent.futures.ProcessPoolExecutor() as executor:
-        #     for number, prime in zip(PRIMES, executor.map(is_prime, PRIMES)):
-        #         print('%d is prime: %s' % (number, prime))
-
-        for rcsb_id, (source_chain,source_residues) in self.source_chains.items():
-            map_motifs(source_chain, self.target_chain, source_residues, self.polymer_class)
-
-        
-
-
-
- 
+        futures = []
+        with ProcessPoolExecutor(max_workers=10) as executor:
+            for rcsb_id, (source_chain, source_residues) in self.source_chains.items():
+                    partial_map_motifs = partial(map_motifs_wrapper, (source_chain, self.target_chain, source_residues, self.polymer_class))
+                    future             = executor.submit(partial_map_motifs)
+                    futures.append((rcsb_id, future))
 
 
-      
+        for rcsb_id, future in futures:
+            _s, _t, tgt_bound_residues = future.result()
+            self.target_mappings[rcsb_id] = tgt_bound_residues
 
 
+        # for rcsb_id, (source_chain,source_residues) in self.source_chains.items():
+        #     _s, _t, tgt_bound_residues    = map_motifs(source_chain, self.target_chain, source_residues, self.polymer_class)
+        #     self.target_mappings[rcsb_id] = tgt_bound_residues
 
-
-
-
+    def get_weights(self):
+        self.target_mappings
+        ...
 
 
